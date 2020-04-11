7f91945c21f4fe7e9e0a1f9f428296a9e7fed36a
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-11 17:32:50 +0700

    Update drills.js

--- scripts/blocks/drills.js
+++ scripts/blocks/drills.js
@@ -6,25 +6,26 @@ const chemicalDrill = extendContent(Drill, "chemical-drill", {
     ];
   },
   draw(tile){
+    const e = tile.entity;
     Draw.rect(Core.atlas.find(this.name + "-bottom"), tile.drawx(), tile.drawy());
-    Draw.color(tile.entity.liquids.current().color);
-    Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
+    Draw.color(e.liquids.current().color);
+    Draw.alpha(e.liquids.total() / this.liquidCapacity);
     Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
     if(this.drawRim){
       Draw.color(this.heatColor);
       Draw.blend(Blending.additive);
-      Draw.alpha(tile.entity.warmup * 0.6 * (1 - 0.3 + Mathf.absin(Time.time(), 3, 0.3)));
+      Draw.alpha(e.warmup * 0.6 * (1 - 0.3 + Mathf.absin(Time.time(), 3, 0.3)));
       Draw.rect(Core.atlas.find(this.name + "-rim"), tile.drawx(), tile.drawy());
       Draw.blend();
     }
     Draw.color();
-    Draw.rect(Core.atlas.find(this.name + "-rotator-bottom"), tile.drawx(), tile.drawy(), tile.entity.drillTime * 2.5);
-    Draw.rect(Core.atlas.find(this.name + "-rotator-top"), tile.drawx(), tile.drawy(), tile.entity.drillTime * -4);
-    Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
-    if(tile.entity.dominantItem != null && this.drawMineItem){
-      Draw.color(tile.entity.dominantItem.color);
+    Draw.rect(Core.atlas.find(this.name + "-rotator-bottom"), tile.drawx(), tile.drawy(), e.drillTime * 2.5);
+    Draw.rect(Core.atlas.find(this.name + "-rotator-top"), tile.drawx(), tile.drawy(), e.drillTime * -4);
+    /*Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
+    if(e.dominantItem != null && this.drawMineItem){
+      Draw.color(e.dominantItem.color);
       Draw.rect("drill-top", tile.drawx(), tile.drawy(), 1);
       Draw.color();
-    }
+    } */
   },
 });

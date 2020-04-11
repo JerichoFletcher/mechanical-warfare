7096e857d3fb4c93d357b2ecdef3ada35b0cbe8a
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-11 18:15:19 +0700

    Update drills.js

--- scripts/blocks/drills.js
+++ scripts/blocks/drills.js
@@ -5,23 +5,31 @@ const chemicalDrill = extendContent(Drill, "chemical-drill", {
       Core.atlas.find(this.name)
     ];
   },
+  load(){
+    this.bottomRegion = Core.atlas.find(this.name + "-bottom")
+    this.liquidRegion = Core.atlas.find(this.name + "-liquid");
+    this.rotatorRegion = Core.atlas.find(this.name + "-rotator-bottom");
+    this.rotatorTopRegion = Core.atlas.find(this.name + "-rotator-top");
+    this.topRegion = Core.atlas.find(this.name + "-top");
+    this.rimRegion = Core.atlas.find(this.name + "-rim");
+  },
   draw(tile){
     const e = tile.entity;
-    Draw.rect(Core.atlas.find(this.name + "-bottom"), tile.drawx(), tile.drawy());
+    Draw.rect(this.bottomRegion, tile.drawx(), tile.drawy());
     Draw.color(e.liquids.current().color);
     Draw.alpha(e.liquids.total() / this.liquidCapacity);
-    Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
+    Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
     if(this.drawRim){
       Draw.color(this.heatColor);
       Draw.blend(Blending.additive);
       Draw.alpha(e.warmup * 0.6 * (1 - 0.3 + Mathf.absin(Time.time(), 3, 0.3)));
-      Draw.rect(Core.atlas.find(this.name + "-rim"), tile.drawx(), tile.drawy());
+      Draw.rect(this.rimRegion, tile.drawx(), tile.drawy());
       Draw.blend();
     }
     Draw.color();
-    Draw.rect(Core.atlas.find(this.name + "-rotator-bottom"), tile.drawx(), tile.drawy(), e.drillTime * 2.5);
-    Draw.rect(Core.atlas.find(this.name + "-rotator-top"), tile.drawx(), tile.drawy(), 1)
-    Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
+    Draw.rect(this.rotatorRegion, tile.drawx(), tile.drawy(), e.drillTime * 2.5);
+    Draw.rect(this.rotatorTopRegion, tile.drawx(), tile.drawy(), );
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
     /*if(e.dominantItem != null && this.drawMineItem){
       Draw.color(e.dominantItem.color);
       Draw.rect("drill-top", tile.drawx(), tile.drawy(), 1);

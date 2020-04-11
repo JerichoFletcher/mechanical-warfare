6694b9e58bb89f9c6820d118c345f404a8589f16
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-11 15:26:46 +0700

    Update production.js

--- scripts/blocks/production.js
+++ scripts/blocks/production.js
@@ -7,8 +7,10 @@ const chemicalStation = extendContent(LiquidConverter, "chemical-station", {
   },
   draw(tile){
     Draw.rect(Core.atlas.find(this.name + "-bottom"), tile.drawx(), tile.drawy());
-    Draw.color(tile.entity.liquids.current().color);
-    Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
+    // Draw.color(tile.entity.liquids.current().color);
+    // Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
+    Draw.color(outputLiquid.liquid.color);
+    Draw.alpha(outputLiquid.amount / this.liquidCapacity);
     Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
     Draw.color();
     Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());

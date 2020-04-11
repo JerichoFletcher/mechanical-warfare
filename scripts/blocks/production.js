dd3d4255b204d2724dec7088533f97ddcb7bc470
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-11 15:09:32 +0700

    Create production.js

---
+++ scripts/blocks/production.js
@@ -0,0 +1,16 @@
+// Chemical Station
+const chemicalStation = extendContent(liquidConverter, "chemical-station", {
+  generateIcons: function(){
+    return [
+      Core.atlas.find(this.name)
+    ];
+  },
+  draw(tile){
+    Draw.rect(Core.atlas.find(this.name + "-bottom"), tile.drawx(), tile.drawy());
+    Draw.color(tile.entity.liquids.current().color);
+    Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
+    Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
+    Draw.color();
+    Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
+  },
+});

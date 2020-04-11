69e286b96e7d5000ef647a39ad363fdcd1747ede
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-11 15:46:40 +0700

    Update production.js

--- scripts/blocks/production.js
+++ scripts/blocks/production.js
@@ -7,10 +7,8 @@ const chemicalStation = extendContent(GenericCrafter, "chemical-station", {
   },
   draw(tile){
     Draw.rect(Core.atlas.find(this.name + "-bottom"), tile.drawx(), tile.drawy());
-    // Draw.color(tile.entity.liquids.current().color);
-    // Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
     Draw.color(this.outputLiquid.liquid.color);
-    Draw.alpha(this.outputLiquid.amount / this.liquidCapacity);
+    Draw.alpha(tile.entity.liquids.get(this.outputLiquid.liquid) / this.liquidCapacity);
     Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
     Draw.color();
     Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());

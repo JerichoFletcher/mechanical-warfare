7fa7ce2bd01e32ec950376ce09858fa9c5eae979
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-11 16:55:21 +0700

    Update drills.js

--- scripts/blocks/drills.js
+++ scripts/blocks/drills.js
@@ -10,14 +10,21 @@ const chemicalDrill = extendContent(Drill, "chemical-drill", {
     Draw.color(tile.entity.liquids.current().color);
     Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
     Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
-    Draw.color(this.heatColor);
-    Draw.blend(Blending.additive);
-    Draw.alpha(tile.entity.warmup * 0.6 * (1 - 0.3 + Mathf.absin(Time.time(), 3, 0.3));
-    Draw.rect(Core.atlas.find(this.name + "-rim"), tile.drawx(), tile.drawy());
-    Draw.blend();
+    if(this.drawRim){
+      Draw.color(this.heatColor);
+      Draw.blend(Blending.additive);
+      Draw.alpha(tile.entity.warmup * 0.6 * (1 - 0.3 + Mathf.absin(Time.time(), 3, 0.3));
+      Draw.rect(Core.atlas.find(this.name + "-rim"), tile.drawx(), tile.drawy());
+      Draw.blend();
+    }
     Draw.color();
     Draw.rect(Core.atlas.find(this.name + "-rotator-bottom"), tile.drawx(), tile.drawy(), tile.entity.drillTime * 2.5);
     Draw.rect(Core.atlas.find(this.name + "-rotator-top"), tile.drawx(), tile.drawy(), tile.entity.drillTime * -4);
     Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
+    if(tile.entity.dominantItem != null && this.drawMineItem){
+      Draw.color(tile.entity.dominantItem.color);
+      Draw.rect("drill-top", tile.drawx(), tile.drawy(), 1);
+      Draw.color();
+    }
   },
 });

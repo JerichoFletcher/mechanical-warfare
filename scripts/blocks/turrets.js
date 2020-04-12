cf5e0c90e6b633e16e77f0507385a3c79767b646
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-04-12 19:35:08 +0700

    Update turrets.js

--- scripts/blocks/turrets.js
+++ scripts/blocks/turrets.js
@@ -1,5 +1,10 @@
 // Aegis
 const aegis = extendContent(ItemTurret, "aegis", {
+  generateIcons: function(){
+    return [
+      Core.atlas.find(this.name + "-icon")
+    ];
+  },
   load(){
     this.super$load();
     this.topRegion = Core.atlas.find(this.name + "-top");
@@ -7,5 +12,32 @@ const aegis = extendContent(ItemTurret, "aegis", {
   drawLayer(tile){
     this.super$drawLayer(tile);
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  },
+});
+
+// Blow
+const blow = extendContent(DoubleTurret, "blow", {
+  generateIcons: function(){
+    return [
+      Core.atlas.find(this.name + "-icon")
+    ];
+  },
+});
+
+// Nighthawk
+const nighthawk = extendContent(ItemTurret, "nighthawk", {
+  generateIcons: function(){
+    return [
+      Core.atlas.find(this.name + "-icon")
+    ];
+  },
+});
+
+// Quake
+const quake = extendContent(ArtilleryTurret, "quake", {
+  generateIcons: function(){
+    return [
+      Core.atlas.find(this.name + "-icon")
+    ];
   },
 });

9d3cb8a1afd1ad540fd8d511bb159a9701a926e2
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-04-12 19:00:29 +0700

    Update turrets.js

--- scripts/blocks/turrets.js
+++ scripts/blocks/turrets.js
@@ -1,1 +1,11 @@
-// empty for now
+// Aegis
+const aegis = extendContent(ItemTurret, "aegis", {
+  load(){
+    this.super$load();
+    this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+  drawLayer(tile){
+    this.super$drawLayer(tile);
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  },
+});

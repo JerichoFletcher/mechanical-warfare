2688b3abc476133dee88262f98916dc9d0ab1fbe
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-13 13:59:53 +0700

    Update drills.js

--- scripts/blocks/drills.js
+++ scripts/blocks/drills.js
@@ -1,10 +1,5 @@
 // Chemical Drill
 const chemicalDrill = extendContent(Drill, "chemical-drill", {
-  generateIcons: function(){
-    return [
-      Core.atlas.find(this.name)
-    ];
-  },
   load(){
     this.baseRegion = Core.atlas.find(this.name + "-base");
     this.liquidRegion = Core.atlas.find(this.name + "-liquid");
@@ -13,6 +8,14 @@ const chemicalDrill = extendContent(Drill, "chemical-drill", {
     this.rotatorRegion = Core.atlas.find(this.name + "-rotator");
     this.topRegion = Core.atlas.find(this.name + "-top");
   },
+  generateIcons: function(){
+    return [
+      Draw.rect(this.baseRegion)
+      Draw.rect(this.region)
+      Draw.rect(this.rotatorRegion)
+      Draw.rect(this.topRegion)
+    ];
+  },
   draw(tile){
     Draw.rect(this.baseRegion, tile.drawx(), tile.drawy());
     Draw.color(tile.entity.liquids.current().color);
@@ -20,26 +23,5 @@ const chemicalDrill = extendContent(Drill, "chemical-drill", {
     Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
     Draw.color();
     this.super$draw(tile);
-    /* const e = tile.ent();
-    Draw.rect(this.bottomRegion, tile.drawx(), tile.drawy());
-    Draw.color(e.liquids.current().color);
-    Draw.alpha(e.liquids.total() / this.liquidCapacity);
-    Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
-    if(this.drawRim){
-      Draw.color(this.heatColor);
-      Draw.blend(Blending.additive);
-      Draw.alpha(e.warmup * 0.6 * (1 - 0.3 + Mathf.absin(Time.time(), 3, 0.3)));
-      Draw.rect(this.rimRegion, tile.drawx(), tile.drawy());
-      Draw.blend();
-    }
-    Draw.color();
-    Draw.rect(this.rotatorBottomRegion, tile.drawx(), tile.drawy(), e.drillTime * 2.5 * e.warmup);
-    Draw.rect(this.rotatorTopRegion, tile.drawx(), tile.drawy(), e.drillTime * -4 * e.warmup);
-    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
-    if(e.dominantItem != null && this.drawMineItem){
-      Draw.color(e.dominantItem.color);
-      Draw.rect("drill-top", tile.drawx(), tile.drawy(), 1);
-      Draw.color();
-    } */
   },
 });

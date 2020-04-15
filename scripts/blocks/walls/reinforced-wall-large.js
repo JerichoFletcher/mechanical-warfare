2bd6310edcd35579e45fb78eba83bb1814eba576
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-15 10:24:29 +0700

    Tweaked reinforced-wall-large mendTimer

--- scripts/blocks/walls/reinforced-wall-large.js
+++ scripts/blocks/walls/reinforced-wall-large.js
@@ -1,10 +1,11 @@
 health = 5120;
 const mendPower = health / 15;
-const mendTimer = 30;
+const mendTimer = 90;
 const reinforcedWallLarge = extendContent(SurgeWall, "reinforced-wall-large", {
   update(tile){
     if (tile.entity.health() < tile.entity.maxHealth() & tile.entity.timer.get(0, mendTimer)) {
       tile.entity.healBy(mendPower);
+      Effects.effect(Fx.healBlockFull, Tmp.c1.set(Color.valueOf("efefff")), tile.drawx(), tile.drawy(), tile.block().size);
     }
   }
 });

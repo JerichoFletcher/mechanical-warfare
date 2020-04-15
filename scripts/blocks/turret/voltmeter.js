bac448f8d216d93a062628fd8e80d42403995015
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-04-15 20:28:21 +0700

    Create voltmeter.js

---
+++ scripts/blocks/turret/voltmeter.js
@@ -0,0 +1,14 @@
+const voltmeter = extendContent(PowerTurret, "voltmeter", {
+  shoot(tile, type){
+    var entity = tile.ent();
+    var result = Predict.intercept(entity, entity.target, type.speed);
+    if (result.isZero()){
+      result.set(entity.target.getX(), entity.target.getY());
+    }
+    var targetRot = result.sub(tile.drawx(), tile.drawy()).angle();
+    entity.heat = 1;
+    Calls.createBullet(type, tile.getTeam(), tile.drawx(), tile.drawy(), targetRot, 1, 1);
+    this.effects(tile);
+    this.useAmmo(tile);
+  },
+});

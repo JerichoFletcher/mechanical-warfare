d43e44c8c9f007e51c9a81c36724c8555b4e349d
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-15 10:25:04 +0700

    Tweaked reinforced-wall mendTimer

--- scripts/blocks/walls/reinforced-wall.js
+++ scripts/blocks/walls/reinforced-wall.js
@@ -1,10 +1,11 @@
 health = 1280;
 const mendPower = health / 15;
-const mendTimer = 30
+const mendTimer = 90
 const reinforcedWall = extendContent(SurgeWall, "reinforced-wall", {
   update(tile) {
     if (tile.entity.health() < tile.entity.maxHealth() & tile.entity.timer.get(0, mendTimer)) {
       tile.entity.healBy(mendPower);
+      Effects.effect(Fx.healBlockFull, Tmp.c1.set(Color.valueOf("efefff")), tile.drawx(), tile.drawy(), tile.block().size);
     }
   }
 });

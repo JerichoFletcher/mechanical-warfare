b2cdf6c8c2559f44cc85f62c43ca2e641cb6c2eb
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-15 08:46:21 +0700

    Create defense.js

---
+++ scripts/blocks/defense.js
@@ -0,0 +1,11 @@
+// Reinforced Wall
+const reinforced-wall = extendContent(Wall, "reinforced-wall",{
+  health = 600
+  const mendPower = health / 15
+  const mendTimer = 30
+  update(tile){
+    if(tile.entity.health() < tile.entity.maxHealth() & tile.entity.timer.get(0, mendTimer)){
+      tile.entity.healBy(mendPower);
+    }
+  }
+});

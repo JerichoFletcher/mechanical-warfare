baa80a22ebb3267ff2aff9e0dd2087cfe8fa41cf
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-15 11:13:09 +0700

    Update reinforced-wall-large.js

--- scripts/blocks/walls/reinforced-wall-large.js
+++ scripts/blocks/walls/reinforced-wall-large.js
@@ -1,10 +1,1 @@
-const reinforcedWallLarge = extendContent(SurgeWall, "reinforced-wall-large", {
-  update(tile){
-    const mendPower = tile.entity.maxHealth() / 15;
-    const mendTimer = 90;
-    if (tile.entity.health() < tile.entity.maxHealth() & tile.entity.timer.get(0, mendTimer)) {
-      tile.entity.healBy(mendPower);
-      Effects.effect(Fx.healBlockFull, Tmp.c1.set(Color.valueOf("efefff")), tile.drawx(), tile.drawy(), tile.block().size);
-    }
-  }
-});
+

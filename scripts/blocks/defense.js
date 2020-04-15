bd7e02eadbd8c4451762442aadee351f888a968d
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-15 08:55:30 +0700

    Update defense.js

--- scripts/blocks/defense.js
+++ scripts/blocks/defense.js
@@ -1,8 +1,8 @@
 // Reinforced Wall
-const reinforced-wall = extendContent(Wall, "reinforced-wall",{
-  health = 1280
-  const mendPower = health / 15
-  const mendTimer = 30
+const reinforced-wall = extendContent(SurgeWall, "reinforced-wall",{
+  health = 1280;
+  const mendPower = health / 15;
+  const mendTimer = 30;
   update(tile){
     if(tile.entity.health() < tile.entity.maxHealth() & tile.entity.timer.get(0, mendTimer)){
       tile.entity.healBy(mendPower);

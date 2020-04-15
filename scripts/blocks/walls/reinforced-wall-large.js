798ae57545ee9407e503b7c485d71b533fdf845f
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-15 09:27:23 +0700

    Create reinforced-wall-large.js

---
+++ scripts/blocks/walls/reinforced-wall-large.js
@@ -0,0 +1,10 @@
+health: 5120;
+const mendPower = health / 15;
+const mendTimer = 30;
+const reinforcedWallLarge = extendContent(SurgeWall, "reinforced-wall-large", {
+  update(tile){
+    if (tile.entity.health() < tile.entity.maxHealth() & tile.entity.timer.get(0, mendTimer)) {
+      tile.entity.healBy(mendPower);
+    }
+  }
+});

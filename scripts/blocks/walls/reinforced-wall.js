2e9ca91f967fb63072878bdeac74b9d5fbff675f
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-15 11:08:58 +0700

    Update reinforced-wall.js

--- scripts/blocks/walls/reinforced-wall.js
+++ scripts/blocks/walls/reinforced-wall.js
@@ -1,8 +1,7 @@
-health = 1280;
-const mendPower = health / 15;
-const mendTimer = 90
 const reinforcedWall = extendContent(SurgeWall, "reinforced-wall", {
   update(tile) {
+    const mendPower = tile.entity.maxHealth() / 15;
+    const mendTimer = 90
     if (tile.entity.health() < tile.entity.maxHealth() & tile.entity.timer.get(0, mendTimer)) {
       tile.entity.healBy(mendPower);
       Effects.effect(Fx.healBlockFull, Tmp.c1.set(Color.valueOf("efefff")), tile.drawx(), tile.drawy(), tile.block().size);

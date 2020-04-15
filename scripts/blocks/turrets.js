488060d491c950a9f5d7047cef271b852bafea99
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-04-15 19:06:24 +0700

    Update turrets.js

--- scripts/blocks/turrets.js
+++ scripts/blocks/turrets.js
@@ -31,8 +31,13 @@ const nighthawk = extendContent(ItemTurret, "nighthawk", {
 const voltmeter = extendContent(PowerTurret, "voltmeter", {
   shoot(tile, type){
     var entity = tile.ent();
+    var result = Predict.intercept(entity, entity.target, type.speed);
+    if (result.isZero()){
+      result.set(entity.target.getX(), entity.target.getY());
+    }
+    var targetRot = result.sub(tile.drawx(), tile.drawy()).angle();
     entity.heat = 1;
-    Calls.createBullet(type, tile.getTeam(), tile.drawx(), tile.drawy(), this.targetRot, 1, 1);
+    Calls.createBullet(type, tile.getTeam(), tile.drawx(), tile.drawy(), targetRot, 1, 1);
     this.effects(tile);
     this.useAmmo(tile);
   },

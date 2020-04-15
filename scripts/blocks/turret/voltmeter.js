56c657a41cf512f7a308bed00b5b2ea80947bc69
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-04-15 20:52:27 +0700

    Update voltmeter.js

--- scripts/blocks/turret/voltmeter.js
+++ scripts/blocks/turret/voltmeter.js
@@ -1,4 +1,10 @@
+var boltrot = [0, 0, 0, 0, 0, 0];
+var boltrotspeed = [16, 12, 18, 15, 24, 20];
+//var warmup = 0;
 const voltmeter = extendContent(PowerTurret, "voltmeter", {
+  update(tile){
+    this.super$update(tile);
+  },
   shoot(tile, type){
     var entity = tile.ent();
     var result = Predict.intercept(entity, entity.target, type.speed);
@@ -10,5 +16,17 @@ const voltmeter = extendContent(PowerTurret, "voltmeter", {
     Calls.createBullet(type, tile.getTeam(), tile.drawx(), tile.drawy(), targetRot, 1, 1);
     this.effects(tile);
     this.useAmmo(tile);
+  },
+  drawLayer(tile){
+    this.super$drawLayer(tile);
+    for (i = 1, i <= 6, i++){
+      var j = i - 1;
+      boltrot[j] += boltrotspeed[j];
+      if (boltrot[j] >= 360) {boltrot[j] -= 360}
+      var s = this.name + "-bolt" + i;
+      Draw.alpha(0.9 + Mathf.absin(Time.time(), boltrotspeed[j] * 0.1, 0.1));
+      Draw.rect(Core.atlas.find(s), tile.drawx(), tile.drawy(), boltrot[j]);
+      Draw.alpha();
+    }
   },
 });

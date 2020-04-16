dd1631fee72d91f29d8e1b26058560bb053400c9
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-16 12:55:19 +0700

    Update offenseUnit.js

--- scripts/units/offenseUnit.js
+++ scripts/units/offenseUnit.js
@@ -1,2 +1,17 @@
 // CopterUnit
 require("units/copterUnit")
+
+// exampleCopterUnit
+const exampleCopterUnit = extendContent(UnitType, "example-copter-unit", {
+  load(){
+    this.super$load();
+    this.rotorBladeRegion = Core.atlas.find(this.name + "-rotor-blade");
+    this.rotorTopRegion = Core.atlas.find(this.name + "-rotor-top");
+    this.rotorSpeed = 9;
+    this.rotorOffset = 4;
+  },
+  UnitType(name, mainConstructor){
+    this(name);
+    this.create(CopterUnit);
+  },
+});

92f94cbb6a2404b866fc1f05094c68d9e25cdc4c
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-16 14:49:53 +0700

    Update offenseUnit.js

--- scripts/units/offenseUnit.js
+++ scripts/units/offenseUnit.js
@@ -1,29 +1,29 @@
 // CopterUnit
-const CopterUnit = extend(HoverUnit, {
-  draw(){
-    Draw.mixcol(Color.white, this.hitTime / this.hitDuration);
-    Draw.rect(this.type.region, this.x, this.y, this.rotation - 90);
-    this.drawWeapons();
-    // draw rotor
-    Draw.rect(this.type.rotorBladeRegion, this.x, this.y - this.type.rotorOffset * Vars.tilesize, Time.time() * this.type.rotorSpeed);
-    Draw.rect(this.type.rotorBladeRegion, this.x, this.y - this.type.rotorOffset * Vars.tilesize, Time.time() * this.type.rotorSpeed * -1);
-    Draw.rect(this.type.rotorTopRegion, this.x, this.y - this.type.rotorOffset * Vars.tilesize);
-    // end draw rotor
-    Draw.mixcol();
-  },
-});
+
 
 // Serpent
 const serpent = extendContent(UnitType, "serpent", {
   create(mainConstructor){
-    this.constructor = mainConstructor;
+    const CopterUnit = extend(HoverUnit, {
+      draw(){
+        Draw.mixcol(Color.white, this.hitTime / this.hitDuration);
+        Draw.rect(this.type.region, this.x, this.y, this.rotation - 90);    
+        this.drawWeapons();
+        // draw rotor
+        Draw.rect(this.type.rotorBladeRegion, this.x, this.y - this.type.rotorOffset * Vars.tilesize, Time.time() * this.type.rotorSpeed);
+        Draw.rect(this.type.rotorBladeRegion, this.x, this.y - this.type.rotorOffset * Vars.tilesize, Time.time() * this.type.rotorSpeed * -1);
+        Draw.rect(this.type.rotorTopRegion, this.x, this.y - this.type.rotorOffset * Vars.tilesize);
+        // end draw rotor
+        Draw.mixcol();
+      },
+    });
+    const cons = new CopterUnit;
+    this.constructor = cons;
     this.description = Core.bundle.getOrNull("unit." + this.name + ".description");
-    this.typeID = new TypeID(this.name, mainConstructor);
+    this.typeID = new TypeID(this.name, cons);
   },
   create: function(team){
-    const unit = this.constructor.get();
-    unit.init(this, team);
-    return unit;
+    this.super$create(team);
   },
   load(){
     this.super$load();

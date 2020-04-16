fa0720bd3eaf5d3b297af513ab0a0cd7226edb79
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-16 13:57:57 +0700

    Update offenseUnit.js

--- scripts/units/offenseUnit.js
+++ scripts/units/offenseUnit.js
@@ -3,6 +3,12 @@ require("units/copterUnit")
 
 // Serpent
 const serpent = extendContent(UnitType, "serpent", {
+  create(mainConstructor){
+    const cons = prov(new CopterUnit);
+    this.constructor = cons;
+    this.description = Core.bundle.getOrNull("unit." + name + ".description");
+    this.typeID = new TypeID(name, cons);
+  },
   load(){
     this.super$load();
     this.rotorBladeRegion = Core.atlas.find(this.name + "-rotor-blade");

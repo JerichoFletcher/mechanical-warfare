3a0ede1ca15c7f6c02ba1ba1840e78fd09e10bab
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-16 13:23:09 +0700

    Update copterUnit.js

--- scripts/units/copterUnit.js
+++ scripts/units/copterUnit.js
@@ -3,7 +3,12 @@ const CopterUnit = extend(HoverUnit, {
     Draw.mixcol(Color.white, this.hitTime / this.hitDuration);
     Draw.rect(this.type.region, this.x, this.y, this.rotation - 90);
     this.drawWeapons();
-    // Draw rotor here
+    // draw rotor
+    for (const i of Mathf.signs) {
+      Draw.rect(this.type.rotorBladeRegion, this.x, this.y - this.type.rotorOffset * Vars.tilesize, Time.time() * this.type.rotorSpeed * i);
+    }
+    Draw.rect(this.type.rotorTopRegion, this.x, this.y - this.type.rotorOffset * Vars.tilesize);
+    // end draw rotor
     Draw.mixcol();
   },
 });

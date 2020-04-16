9ccc42034c6258908d56039ad18a275fff688384
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-16 16:59:32 +0700

    Update voltmeter.js

--- scripts/blocks/turret/voltmeter.js
+++ scripts/blocks/turret/voltmeter.js
@@ -1,4 +1,5 @@
 const boltrotspeed = [8, 6, 18, 15, 24, 20];
+const boltrotdir = [1, -1, -1, 1, 1, -1];
 const boltCooldown = 0.05;
 //var warmup = 0;
 const voltmeter = extendContent(PowerTurret, "voltmeter", {
@@ -24,10 +25,16 @@ const voltmeter = extendContent(PowerTurret, "voltmeter", {
     for (i = 1; i <= 6; i++){
       const j = i - 1;
       const s = this.name + "-bolt" + i;
+      const rawrot = Time.time() * boltrotspeed[j] * boltrotdir[j];
+      const truerot =
+        rawrot < 0 ?
+          360 + (rawrot % 360)
+        :
+          rawrot % 360;
       Draw.mixcol(Color.white, Mathf.absin(Time.time(), boltrotspeed[j] * 0.1, 0.5));
       Draw.alpha(entity.power.status * (0.9 + Mathf.absin(Time.time(), boltrotspeed[j] * 0.1, 0.1)));
       Draw.blend(Blending.additive);
-      Draw.rect(Core.atlas.find(s), tile.drawx(), tile.drawy(), Time.time() * boltrotspeed[j] * (-1 ** i));
+      Draw.rect(Core.atlas.find(s), tile.drawx(), tile.drawy(), truerot);
       Draw.blend();
       Draw.reset();
     }

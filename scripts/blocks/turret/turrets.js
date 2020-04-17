c804477e6ae9951753f1b5ffa7783f3d93d668ac
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-04-17 17:52:19 +0700

    Create turrets.js

---
+++ scripts/blocks/turret/turrets.js
@@ -0,0 +1,22 @@
+// Aegis
+const aegis = extendContent(ItemTurret, "aegis", {
+  
+});
+
+// Blow
+const blow = extendContent(DoubleTurret, "blow", {
+  
+});
+
+// Nighthawk
+const nighthawk = extendContent(ItemTurret, "nighthawk", {
+  
+});
+
+//Voltmeter
+require("blocks/turret/voltmeter")
+
+// Quake
+const quake = extendContent(ArtilleryTurret, "quake", {
+  
+});

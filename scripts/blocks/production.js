8f8b264afbfd8c19de63d10e0680fd317caa3b83
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-17 14:07:53 +0700

    Added stone-centrifuge rendering scripts

--- scripts/blocks/production.js
+++ scripts/blocks/production.js
@@ -20,3 +20,18 @@ const chemicalStation = extendContent(GenericCrafter, "chemical-station", {
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
   },
 });
+
+// Stone Centrifuge
+const stoneCentrifuge = extendContent(Genericrafter, "stone-centrifuge", {
+  load(){
+    this.super$load();
+    this.liquidRegion = Core.atlas.find(this.name + "-liquid");
+  },
+  draw(tile){
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+    Draw.color(tile.entity.liquids.current().color);
+    Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
+    Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
+    Draw.color();
+  }
+});

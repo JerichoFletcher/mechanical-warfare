102143cedf3ec8a1b1f4c26c5127be0b305d192e
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-18 13:19:32 +0700

    Update production.js

--- scripts/blocks/production.js
+++ scripts/blocks/production.js
@@ -1,18 +1,18 @@
 // Chemical Station
 const chemicalStation = extendContent(GenericCrafter, "chemical-station", {
   load(){
-    this.bottomRegion = Core.atlas.find(this.name + "-bottom");
+    this.region = Core.atlas.find(this.name);
     this.liquidRegion = Core.atlas.find(this.name + "-liquid");
     this.topRegion = Core.atlas.find(this.name + "-top");
   },
   generateIcons: function(){
     return [
-      Core.atlas.find(this.name + "-bottom"),
+      Core.atlas.find(this.name),
       Core.atlas.find(this.name + "-top")
     ];
   },
   draw(tile){
-    Draw.rect(this.bottomRegion, tile.drawx(), tile.drawy());
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
     Draw.color(this.outputLiquid.liquid.color);
     Draw.alpha(tile.entity.liquids.get(this.outputLiquid.liquid) / this.liquidCapacity);
     Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
@@ -26,6 +26,7 @@ const stoneCentrifuge = extendContent(GenericCrafter, "stone-centrifuge", {
   load(){
     this.super$load();
     this.liquidRegion = Core.atlas.find(this.name + "-liquid");
+    this.topRegion = Core.atlas.find(this.name + "-top");
   },
   draw(tile){
     Draw.rect(this.region, tile.drawx(), tile.drawy());
@@ -33,5 +34,6 @@ const stoneCentrifuge = extendContent(GenericCrafter, "stone-centrifuge", {
     Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
     Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
     Draw.color();
-  }
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  },
 });

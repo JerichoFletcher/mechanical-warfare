feaa5c6cbe43dc8c7dcec75e33f3166c750dc624
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-19 12:35:25 +0700

    Created stone grinder rendering scripts

--- scripts/blocks/production.js
+++ scripts/blocks/production.js
@@ -24,9 +24,15 @@ const chemicalStation = extendContent(GenericCrafter, "chemical-station", {
 // Stone Centrifuge
 const stoneCentrifuge = extendContent(GenericCrafter, "stone-centrifuge", {
   load(){
-    this.super$load();
+    this.region = Core.atlas.find(this.name)
     this.liquidRegion = Core.atlas.find(this.name + "-liquid");
     this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+  generateIcons: function(){
+    return [
+      Core.atlas.find(this.name),
+      Core.atlas.find(this.name + "-top")
+    ];
   },
   draw(tile){
     Draw.rect(this.region, tile.drawx(), tile.drawy());
@@ -34,6 +40,27 @@ const stoneCentrifuge = extendContent(GenericCrafter, "stone-centrifuge", {
     Draw.alpha(tile.entity.liquids.total() / this.liquidCapacity);
     Draw.rect(this.liquidRegion, tile.drawx(), tile.drawy());
     Draw.color();
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  },
+});
+
+// Stone Grinder
+const stoneGrinder = extendContent(GenericCrafter, "stone-grinder", {
+  load(){
+    this.bottomRegion = Core.atlas.find(this.name + "-bottom");
+    this.rightRotatorRegion = Core.atlas.find(this.name + "-rotator1");
+    this.leftRotatorRegion = Core.atlas.find(this.name + "-rotator2");
+    this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+  generateIcons: function(){
+    return [
+      Core.atlas.find(this.name)
+    ];
+  },
+  draw(tile){
+    Draw.rect(this.bottomRegion, tile.drawx(), tile.drawy());
+    Draw.rect(this.rightRotatorRegion, tile.drawx() + 11, tile.drawy() + 11, tile.entity.totalProgress * 2);
+    Draw.rect(this.leftRotatorRegion, tile.drawx() - 10, tile.drawy() - 10, tile.entity.totalProgress * -2);
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
   },
 });

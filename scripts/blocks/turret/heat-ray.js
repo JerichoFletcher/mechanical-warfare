045c8b26427fb19439148cd4ecc365c08db5c9a4
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-04-22 20:32:51 +0700

    Update heat-ray.js

--- scripts/blocks/turret/heat-ray.js
+++ scripts/blocks/turret/heat-ray.js
@@ -3,8 +3,10 @@ const rayScale = 0.9;
 const heatRay = extendContent(PowerTurret, "heat-ray", {
   load(){
     this.super$load();
-    this.beamRegion = Core.atlas.find(this.name + "-beam");
-    this.beamEndRegion = Core.atlas.find(this.name + "-beam-end");
+    //this.beamRegion = Core.atlas.find(this.name + "-beam");
+    //this.beamEndRegion = Core.atlas.find(this.name + "-beam-end");
+    this.beamRegion = Core.atlas.find("laser");
+    this.beamEndRegion = Core.atlas.find("laser-end");
   },
   update(tile){
     var entity = tile.ent();
@@ -58,7 +60,7 @@ const heatRay = extendContent(PowerTurret, "heat-ray", {
       var angle = targetPos.sub(tile.drawx(), tile.drawy()).angle();
       if (Angles.angleDist(entity.rotation, angle) < this.shootCone){
         Draw.color(Color.lightGray, Color.white, 1 - Mathf.absin(Time.time(), 0.8, 0.3));
-        Drawf.laser(this.beamRegion, this.beamEndRegion, this.tr.x, this.tr.y, entity.target.getX(), entity.target.getY());
+        Drawf.laser(this.beamRegion, this.beamEndRegion, this.tr.x, this.tr.y, entity.target.getX(), entity.target.getY(), entity.heat);
         Draw.color();
       }
     }

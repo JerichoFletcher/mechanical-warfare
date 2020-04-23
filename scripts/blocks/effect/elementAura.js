facc73c4f424fab34cae4e382188fe45906f75ad
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-23 20:36:35 +0700

    Update elementAura.js

--- scripts/blocks/effect/elementAura.js
+++ scripts/blocks/effect/elementAura.js
@@ -1,2 +1,20 @@
+// Fire Aura effect
+const fireAuraColor = "ffaa44";
+const fireAuraEffect = newEffect(40, e => {
+  Draw.color(Color.valueOf(fireAuraColor));
+  Lines.stroke(e.fout() * 3);
+  Lines.circle(e.x, e.y, e.fin() * 50);
+});
+
 // Fire Aura
-require("blocks/effect/fire-aura")
+const fireAura = extendContent(LiquidTurret, "fire-aura", {
+  load(tile){
+    this.region = Core.atlas.find(this.name);
+    this.liquidRegion = Core.atlas.find(this.name + "-liquid");
+    this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+  drawLayer(tile){
+    this.super$drawLayer(tile);
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  },
+});

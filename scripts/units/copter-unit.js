59d273e02774711e59165d48b5808edc3f0400a7
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-29 19:28:47 +0700

    Update copter-unit.js

--- scripts/units/copter-unit.js
+++ scripts/units/copter-unit.js
@@ -5,8 +5,8 @@ const copterBase = prov(() => extend(HoverUnit, {
     const rotorSpeed = 3;
     var offx = Angles.trnsx(this.rotation, offset);
     var offy = Angles.trnsy(this.rotation, offset);
-    var rotorBladeRegion = Core.atlas.isFound(this.type.rotorBladeRegion) ? this.type.rotorBladeRegion : Core.atlas.find("clear");
-    var rotorTopRegion = Core.atlas.isFound(this.type.rotorTopRegion) ? this.type.rotorTopRegion : Core.atlas.find("clear");
+    var rotorBladeRegion = Core.atlas.find(this.type.name + "-rotor-blade");
+    var rotorTopRegion = Core.atlas.find(this.type.name + "-rotor-top");
     Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * rotorSpeed);
     Draw.rect(rotorTopRegion, this.x + offx, this.y + offy);
   },
@@ -18,13 +18,13 @@ const serpentUnit = extendContent(UnitType, "serpent", {
     this.weapon.load();
     //this.region = Core.atlas.find(this.name);
     this.region = Core.atlas.find("revenant");
-    this.rotorBladeRegion = Core.atlas.find(this.name + "-rotor-blade");
+    /*this.rotorBladeRegion = Core.atlas.find(this.name + "-rotor-blade");
     this.rotorTopRegion = Core.atlas.find(this.name + "-rotor-top");
     if(!Core.atlas.isFound(this.rotorBladeRegion) || !Core.atlas.isFound(this.rotorTopRegion)){
       print("the error, damn you donkey!");
     }else{
       print("gud boi sprite has been found");
-    }
+    }*/
   },
   generateIcons: function(){
     return [Core.atlas.find("revenant")];

8dc1586033097f801d515e05a82e3ddf428f161a
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-29 18:54:25 +0700

    Update copter-unit.js

--- scripts/units/copter-unit.js
+++ scripts/units/copter-unit.js
@@ -1,11 +1,14 @@
 const copterBase = prov(() => extend(HoverUnit, {
   draw(){
     this.super$draw();
-    var offset = 1;
+    const offset = 1;
+    const rotorSpeed = 3;
     var offx = Angles.trnsx(this.rotation, offset);
     var offy = Angles.trnsy(this.rotation, offset);
-    Draw.rect(this.type.rotorBladeRegion, this.x + offx, this.y + offy, Time.time());
-    Draw.rect(this.type.rotorTopRegion, this.x + offx, this.y + offy);
+    var rotorBladeRegion = this.type.rotorBladeRegion != Core.atlas.find("error") ? this.type.rotorBladeRegion : Core.atlas.find("mechanical-warfare-rotor-blade");
+    var rotorTopRegion = this.type.rotorTopRegion != Core.atlas.find("error") ? this.type.rotorTopRegion : Core.atlas.find("mechanical-warfare-rotor-top");
+    Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * rotorSpeed);
+    Draw.rect(rotorTopRegion, this.x + offx, this.y + offy);
   },
 }));
 
@@ -17,6 +20,9 @@ const serpentUnit = extendContent(UnitType, "serpent", {
     this.region = Core.atlas.find("revenant");
     this.rotorBladeRegion = Core.atlas.find("mechanical-warfare-rotor-blade");
     this.rotorTopRegion = Core.atlas.find("mechanical-warfare-rotor-top");
+  },
+  generateIcons: function(){
+    return [Core.atlas.find("revenant")];
   },
 });
 serpentUnit.create(copterBase);

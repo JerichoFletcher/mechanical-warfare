6afb5964803adf5290d6412e78e33b45eae8076e
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-29 19:57:00 +0700

    Update copter-unit.js

--- scripts/units/copter-unit.js
+++ scripts/units/copter-unit.js
@@ -1,24 +1,23 @@
 const copterBase = prov(() => extend(HoverUnit, {
   draw(){
     this.super$draw();
-    const offset = 1;
-    const rotorSpeed = 15;
-    var offx = Angles.trnsx(this.rotation, offset);
-    var offy = Angles.trnsy(this.rotation, offset);
+    var offx = Angles.trnsx(this.rotation, this.type.rotorOffset());
+    var offy = Angles.trnsy(this.rotation, this.type.rotorOffset());
     var rotorBladeRegion = Core.atlas.find(this.type.name + "-rotor-blade");
     var rotorTopRegion = Core.atlas.find(this.type.name + "-rotor-top");
     if(Core.atlas.isFound(rotorBladeRegion) && Core.atlas.isFound(rotorTopRegion)){
       var width = rotorBladeRegion.getWidth() * this.type.rotorScale();
       var height = rotorBladeRegion.getHeight() * this.type.rotorScale();
       for(var i = 0; i < 3; i++){
-      Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * rotorSpeed);
-      Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * -rotorSpeed);
+      Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * this.type.rotorSpeed());
+      Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * -this.type.rotorSpeed());
       Draw.rect(rotorTopRegion, this.x + offx, this.y + offy);
       }
     }
   },
 }));
 
+print(modName);
 // Serpent
 const serpentUnit = extendContent(UnitType, "serpent", {
   load(){
@@ -27,6 +26,12 @@ const serpentUnit = extendContent(UnitType, "serpent", {
   },
   rotorScale: function(){
     return 1.4;
+  },
+  rotorSpeed: function(){
+    return 15;
+  },
+  rotorOffset: function(){
+    return 1;
   },
 });
 serpentUnit.create(copterBase);

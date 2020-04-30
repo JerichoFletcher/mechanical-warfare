6822be3f7cc4a8564c3e8c71345a023d1c9b5a28
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-30 08:12:30 +0700

    Update copter-unit.js

--- scripts/units/copter-unit.js
+++ scripts/units/copter-unit.js
@@ -3,18 +3,18 @@ const copterBase = prov(() => extend(HoverUnit, {
     this.super$draw();
     var offx = Angles.trnsx(this.rotation, this.type.rotorOffset());
     var offy = Angles.trnsy(this.rotation, this.type.rotorOffset());
-    /*var rotorBladeRegion = Core.atlas.isFound(this.type.rotorBladeRegion()) ?
+    var rotorBladeRegion = Core.atlas.isFound(this.type.rotorBladeRegion()) ?
       this.type.rotorBladeRegion() : Core.atlas.find(modName + "-rotor-blade");
     var rotorTopRegion = Core.atlas.isFound(this.type.rotorTopRegion()) ?
-      this.type.rotorTopRegion() : Core.atlas.find(modName + "-rotor-top");*/
-    var rotorBladeRegion = Core.atlas.find(modName + "-rotor-blade");
-    var rotorTopRegion = Core.atlas.find(modName + "-rotor-top");
+      this.type.rotorTopRegion() : Core.atlas.find(modName + "-rotor-top");
     if(Core.atlas.isFound(rotorBladeRegion) && Core.atlas.isFound(rotorTopRegion)){
       var width = rotorBladeRegion.getWidth() * this.type.rotorScale();
       var height = rotorBladeRegion.getHeight() * this.type.rotorScale();
+      Draw.mixcol(Color.white, this.hitTime / this.hitDuration);
       Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * this.type.rotorSpeed());
       Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, 90 + Time.time() * this.type.rotorSpeed());
       Draw.rect(rotorTopRegion, this.x + offx, this.y + offy);
+      Draw.mixcol();
     }
   },
 }));
@@ -24,14 +24,14 @@ const serpentUnit = extendContent(UnitType, "serpent", {
   load(){
     this.weapon.load();
     this.region = Core.atlas.find("revenant");
-    this.bladeRegion = Core.atlas.find(this.name + "-rotor-blade");
-    this.topRegion = Core.atlas.find(this.name + "-rotor-top");
+    //this.bladeRegion = Core.atlas.find(this.name + "-rotor-blade");
+    //this.topRegion = Core.atlas.find(this.name + "-rotor-top");
   },
   rotorBladeRegion: function(){
-    return this.bladeRegion;
+    return typeof(this.bladeRegion) !== "undefined" ? this.bladeRegion : Core.atlas.find("error");
   },
   rotorTopRegion: function(){
-    return this.topRegion;
+    return typeof(this.topRegion) !== "undefined" ? this.topRegion : Core.atlas.find("error");
   },
   rotorScale: function(){
     return 1.4;
@@ -47,8 +47,8 @@ serpentUnit.create(copterBase);
 
 const serpentFactory = extendContent(UnitFactory, "serpent-factory", {
   load(){
-    this.region = Core.atlas.find("revenant-factory");
-    this.topRegion = Core.atlas.find("revenant-factory-top");
+    this.region = Core.atlas.find(modName + "-phantasm-factory");
+    this.topRegion = Core.atlas.find(modName + "-phantasm-factory-top");
   }
 });
 serpentFactory.unitType = serpentUnit;

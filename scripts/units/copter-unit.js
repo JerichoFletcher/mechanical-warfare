04062277a2331658dfee958e9f3765fb41b4970c
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-04-30 07:56:46 +0700

    Update copter-unit.js

--- scripts/units/copter-unit.js
+++ scripts/units/copter-unit.js
@@ -3,15 +3,17 @@ const copterBase = prov(() => extend(HoverUnit, {
     this.super$draw();
     var offx = Angles.trnsx(this.rotation, this.type.rotorOffset());
     var offy = Angles.trnsy(this.rotation, this.type.rotorOffset());
-    var rotorBladeRegion = Core.atlas.isFound(this.type.rotorBladeRegion()) ?
+    /*var rotorBladeRegion = Core.atlas.isFound(this.type.rotorBladeRegion()) ?
       this.type.rotorBladeRegion() : Core.atlas.find(modName + "-rotor-blade");
     var rotorTopRegion = Core.atlas.isFound(this.type.rotorTopRegion()) ?
-      this.type.rotorTopRegion() : Core.atlas.find(modName + "-rotor-top");
+      this.type.rotorTopRegion() : Core.atlas.find(modName + "-rotor-top");*/
+    var rotorBladeRegion = Core.atlas.find(modName + "-rotor-blade");
+    var rotorTopRegion = Core.atlas.find(modName + "-rotor-top");
     if(Core.atlas.isFound(rotorBladeRegion) && Core.atlas.isFound(rotorTopRegion)){
       var width = rotorBladeRegion.getWidth() * this.type.rotorScale();
       var height = rotorBladeRegion.getHeight() * this.type.rotorScale();
       Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * this.type.rotorSpeed());
-      Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, Time.time() * -this.type.rotorSpeed());
+      Draw.rect(rotorBladeRegion, this.x + offx, this.y + offy, 90 + Time.time() * this.type.rotorSpeed());
       Draw.rect(rotorTopRegion, this.x + offx, this.y + offy);
     }
   },

63b7d76783f9c9c7d11be1dd8cede4c134821768
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-05-04 17:32:26 +0700

    Update copter-base.js

--- scripts/units/copter-base.js
+++ scripts/units/copter-base.js
@@ -1,5 +1,42 @@
 module.exports = {
-  drawRotor(bladeRegion, topRegion, scale, speed, offset, width, alter, twinBlade){
-    
-  }
+  drawRotor(base){
+    var bladeRegion = Core.atlas.isFound(base.type.rotorBladeRegion()) ?
+      base.type.rotorBladeRegion() : Core.atlas.find(modName + "-rotor-blade");
+    var topRegion = Core.atlas.isFound(base.type.rotorTopRegion()) ?
+      base.type.rotorTopRegion() : Core.atlas.find(modName + "-rotor-top");
+    var offx = Angles.trnsx(base.rotation, base.type.rotorOffset(), base.type.rotorWidth());
+    var offy = Angles.trnsy(base.rotation, base.type.rotorOffset(), base.type.rotorWidth());
+    var w = bladeRegion.getWidth() * base.type.rotorScale();
+    var h = bladeRegion.getHeight() * base.type.rotorScale();
+    var angle = Time.time() * base.type.rotorSpeed() % 360;
+    var angle2 = base.type.alternateRotor() ? 360 - (angle % 360) : 90 + angle;
+    if(base.type.isTwinBlade()){
+      for(var i = 0; i < 2; i++){
+        var sign = Mathf.signs[i];
+        var twinOffX = offx * sign;
+        var twinOffY = offy * sign;
+        Draw.rect(bladeRegion, base.x + twinOffX, base.y + twinOffY, w, h, angle);
+        Draw.rect(bladeRegion, base.x + twinOffX, base.y + twinOffY, w, h, angle2);
+        Draw.rect(topRegion, base.x + twinOffX, base.y + twinOffY);
+      }
+    }else{
+      Draw.rect(bladeRegion, base.x + offx, base.y + offy, w, h, angle);
+      Draw.rect(bladeRegion, base.x + offx, base.y + offy, w, h, angle2);
+      Draw.rect(topRegion, base.x + offx, base.y + offy);
+    }
+  },
+  drawWeapons(base){
+    for(var i = 0; i <= 1; i++){
+      var sign = Mathf.signs[i];
+      var angle = base.rotation - 90;
+      var trY = base.type.offsetY - base.type.weapon.getRecoil(base, (sign > 0));
+      var w = -sign * base.type.weapon.region.getWidth() * Draw.scl;
+      var h = base.type.weapon.region.getHeight() * Draw.scl;
+      Draw.rect(base.type.weapon.region, 
+        base.x + Angles.trnsx(angle, base.getWeapon().width * sign, trY),
+        base.y + Angles.trnsy(angle, base.getWeapon().width * sign, trY),
+        w, h, angle
+      );
+    }
+  },
 };

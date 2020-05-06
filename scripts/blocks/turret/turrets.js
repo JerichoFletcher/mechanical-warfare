d1992354ab93aae645e6b94d11b9d3a074c086d7
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-05-06 07:59:31 +0700

    Update turrets.js

--- scripts/blocks/turret/turrets.js
+++ scripts/blocks/turret/turrets.js
@@ -15,9 +15,7 @@ const nighthawk = extendContent(ItemTurret, "nighthawk", {
 require("blocks/turret/voltmeter")
 
 // Blow
-const blow = extendContent(DoubleTurret, "blow", {
-  
-});
+require("blocks/turret/blow")
 
 // Quake
 const quake = extendContent(ArtilleryTurret, "quake", {

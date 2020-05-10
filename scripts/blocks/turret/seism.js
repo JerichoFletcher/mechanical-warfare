edfd7288d7cd3921e87b01cd64fabeeb21a0fdd1
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-05-10 17:31:11 +0700

    Update seism.js

--- scripts/blocks/turret/seism.js
+++ scripts/blocks/turret/seism.js
@@ -1,6 +1,23 @@
 const seism = extendContent(ArtilleryTurret, "seism", {
   load(){
     this.super$load();
-    this.layer2 = Layer.power;
+    for(var i = 0; i <= 2; i++){
+      this.ammoRegions[i] = Core.atlas.find(this.name + "-phase" + i);
+    }
+  },
+  drawLayer(tile){
+    this.super$load();
+    var entity = tile.ent();
+    var val = entity.totalAmmo / this.maxAmmo;
+    for(var i = 0; i <= 2; i++){
+      var j = i + 1;
+      var lo = i / 3;
+      var hi = j / 3;
+      Draw.color(Color.valueOf("00000000"), Pal.turretHeat, (Mathf.clamp(val, lo, hi) - lo) * 3);
+      Draw.blend(Blending.additive);
+      Draw.rect(this.ammoRegions[i], tile.drawx() + this.tr2.x, tile.drawy() + this.tr2.y, entity.rotation - 90);
+      Draw.blend();
+      Draw.color();
+    }
   }
 });

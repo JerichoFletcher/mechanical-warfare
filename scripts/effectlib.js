b84818c8d8b4c6c985620f8660d04255be16df46
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-05-11 19:23:26 +0700

    Update effectlib.js

--- scripts/effectlib.js
+++ scripts/effectlib.js
@@ -7,15 +7,39 @@ module.exports = {
     });
     return trail;
   },
+  shockCircle(x, y, col, thickness, radius){
+    Draw.color(col);
+    Lines.stroke(thickness);
+    Lines.circle(x, y, radius);
+    Draw.color();
+    Lines.stroke(1);
+  },
+  fillCircle(x, y, col, alpha, radius){
+    Draw.color(col);
+    Draw.alpha(alpha);
+    Fill.circle(x, y, radius);
+    Draw.color();
+  },
+  splashLines(cx, cy, col, thickness, distance, length, count, seed){
+    Draw.color(col);
+    Lines.stroke(thickness);
+    Angles.randLenVectors(seed, count, distance, new Floatc2(){get: (x, y) => {
+      Lines.lineAngle(cx + x, cy + u, Mathf.angle(offx, offy), length);
+    }});
+    Draw.color();
+    Lines.stroke(1);
+  },
   quakeHit: function(lifetime, cColor, cThickness, cStartSize, cEndSize, c2Color, c2StartSize, c2EndSize, lColor, lThickness, lCount, lStart, lEnd, lStartSize, lEndSize){
     const hit = newEffect(lifetime, e => {
-      Draw.color(cColor);
+      /*Draw.color(cColor);
       Lines.stroke(cThickness * e.fout());
-      Lines.circle(e.x, e.y, e.fin() * (cEndSize - cStartSize) + cStartSize);
+      Lines.circle(e.x, e.y, e.fin() * (cEndSize - cStartSize) + cStartSize);*/
+      this.splashCircle(e.x, e.y, cColor, cThickness * e.fout(), Mathf.lerp(cStartSize, cEndSize, e.fin()));
       
-      Draw.color(c2Color);
+      /*Draw.color(c2Color);
       Draw.alpha(e.fin() * 0.7 + 0.3);
-      Fill.circle(e.x, e.y, e.fout() * (c2StartSize - c2EndSize) + c2EndSize);
+      Fill.circle(e.x, e.y, e.fout() * (c2StartSize - c2EndSize) + c2EndSize);*/
+      this.fillCircle(e.x, e.y, c2Color, e.fin() * 0.7 + 0.3, Mathf.lerp(c2StartSize, c2EndSize, e.fout()));
       
       Draw.color(lColor);
       Lines.stroke(e.fout() * lThickness);

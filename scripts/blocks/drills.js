3f4a4a80be578f28ee496eeacb8d42f4a81dcf30
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-05-14 16:24:20 +0700

    Update drills.js

--- scripts/blocks/drills.js
+++ scripts/blocks/drills.js
@@ -32,11 +32,11 @@ Vars.content.blocks().each(boolf(b => b instanceof Drill), cons(b => {
     b.returnCount = 0;
     b.oreCount.clear();
     b.itemArray.clear();
-    tile.getLinkedTilesAs(b, b.tempTiles).each(cons((other) => {
+    tile.getLinkedTilesAs(b, b.tempTiles).each(cons(other => {
       if(b.isValid(other)){
         b.oreCount.getAndIncrement(b.getDrop(other), 0, 1);
       }
-    }}));
+    }));
     b.oreCount.keys().toArray().each(cons(item => {
       b.itemArray.add(item);
     }));

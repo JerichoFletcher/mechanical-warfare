830e420814e55fc4973385d5206be488c60a3770
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-05-14 15:58:10 +0700

    Update drills.js

--- scripts/blocks/drills.js
+++ scripts/blocks/drills.js
@@ -27,5 +27,31 @@ const chemicalDrill = extendContent(Drill, "chemical-drill", {
 });
 
 Vars.content.blocks().each(boolf(b => b instanceof Drill), cons(b => {
-  
+  b.countOre = function(tile){
+    b.returnItem = null;
+    b.returnCount = 0;
+    b.oreCount.clear();
+    b.itemArray.clear();
+    for(const other of tile.getLinkedTilesAs(b, b.tempTiles)){
+      if(b.isValid(other)){
+        b.oreCount.getAndIncrement(b.getDrop(other), 0, 1);
+      }
+    }
+    for(const item of b.oreCount.keys()){
+      b.itemArray.add(item);
+    }
+    const stone = Vars.content.getByName(ContentType.item, modName + "-cobblestone");
+    b.itemArray.sort((item1, item2) => {
+      var type = Boolean.compare(item1 != Items.sand || item1 != stone, item2 != Items.sand || item2 != stone);
+      if(type != 0){return type;}
+      var amounts = Integer.compare(b.oreCount.get(item1, 0), b.oreCount.get(item2, 0));
+      if(amounts != 0){return amounts;}
+      return Integer.compare(item1.id, item2.id);
+    });
+    if(b.itemArray.size == 0){
+      return;
+    }
+    b.returnItem = b.itemArray.peek();
+    b.returnCount = b.oreCount.get(b.itemArray.peek(), 0);
+  }
 });

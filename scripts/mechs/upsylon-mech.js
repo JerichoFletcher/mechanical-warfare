e4134c3d293119d9fc4939ddaff3a979d00cb1ba
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-05-14 20:06:32 +0700

    Update upsylon-mech.js

--- scripts/mechs/upsylon-mech.js
+++ scripts/mechs/upsylon-mech.js
@@ -32,6 +32,23 @@ shotgun.velocityRnd = 0.5;
 shotgun.shootSound = Sounds.shootBig;
 shotgun.bullet = shotgunRound;
 
+const upsylonPlasma = extend(BasicBulletType, {});
+upsylonPlasma.splashDamage = Vars.state.rules.playerDamageMultiplier * 27;
+upsylonPlasma.splashDamageRadius = 20;
+upsylonPlasma.speed = 12;
+upsylonPlasma.lifetime = 60;
+upsylonPlasma.drag = 0.05;
+upsylonPlasma.shootEffect = Fx.shootBig;
+upsylonPlasma.smokeEffect = Fx.shootBigSmoke;
+upsylonPlasma.trailEffect = newEffect(30, e => {
+  elib.fillCircle(e.x, e.y, Pal.lancerLaser, 1, Mathf.lerp(6, 0.2, e.fin()));
+});
+upsylonPlasma.hitEffect = newEffect(20, e => {
+  elib.outlineCircle(e.x, e.y, Pal.lancerLaser, e.fout() * 6, 1 + e.fin() * (upsylonPlasma.splashDamageRadius + 4));
+  elib.fillCircle(e.x, e.y, Pal.lancerLaser, 0.2 + e.fout() * 0.8, Mathf.lerp(upsylonPlasma.splashDamageRadius, 0.2, e.fin()));
+});
+upsylonPlasma.despawnEffect = upsylonPlasma.hitEffect;
+
 const upsylon = extendContent(Mech, "upsylon-mech", {
   load(){
     this.weapon.load();
@@ -44,7 +61,7 @@ const upsylon = extendContent(Mech, "upsylon-mech", {
         var i = Mathf.signs[this._shots % 2];
         this.pl1.trns(player.rotation - 90, i * this.weaponOffsetX, this.weaponOffsetY);
         var dir = player.rotation + i * this.secondaryAngle;
-        Calls.createBullet(BulletType, player.getTeam(),
+        Calls.createBullet(upsylonPlasma, player.getTeam(),
           player.x + this.pl1.x, player.y + this.pl1.y,
           dir, 1, 1
         );

bbbaa853fc3e3adf87219b722bd50e99967dc6e4
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-05-15 21:19:20 +0700

    Update effectlib.js

--- scripts/effectlib.js
+++ scripts/effectlib.js
@@ -9,9 +9,9 @@ module.exports = {
     Lines.stroke(1);
   },
   outlineCircle(x, y, thickness, radius){
-		Lines.stroke(thickness);
-		Lines.circle(x, y, radius);
-		Lines.stroke(1);
+    Lines.stroke(thickness);
+    Lines.circle(x, y, radius);
+    Lines.stroke(1);
 	},
   fillCircle(x, y, col, alpha, radius){
     Draw.color(col);
@@ -31,13 +31,13 @@ module.exports = {
     Draw.color();
     Lines.stroke(1);
   },
-	splashLines(x, y, thickness, distance, length, count, seed){
-		Lines.stroke(thickness);
-		Angles.randLenVectors(seed, count, distance, new Floatc2(){get: (a, b) => {
-			Lines.lineAngle(x + a, y + b, Mathf.angle(a, b), length);
-		}});
-		Lines.stroke(1);
-	},
+  splashLines(x, y, thickness, distance, length, count, seed){
+    Lines.stroke(thickness);
+    Angles.randLenVectors(seed, count, distance, new Floatc2(){get: (a, b) => {
+      Lines.lineAngle(x + a, y + b, Mathf.angle(a, b), length);
+    }});
+    Lines.stroke(1);
+  },
   splashCircles(x, y, col, alpha, radius, distance, count, seed){
     Angles.randLenVectors(seed, count, distance, new Floatc2(){get: (a, b) => {
       this.fillCircle(x + a, y + b, col, alpha, radius);

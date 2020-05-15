d749bd04ed7f82ea2da3bca089bcf959bb79b3df
JerichoFletcher <62737325+JerichoFletcher@users.noreply.github.com>
2020-05-15 21:17:19 +0700

    Overload methods without color arg

--- scripts/effectlib.js
+++ scripts/effectlib.js
@@ -8,12 +8,20 @@ module.exports = {
     Draw.color();
     Lines.stroke(1);
   },
+  outlineCircle(x, y, thickness, radius){
+		Lines.stroke(thickness);
+		Lines.circle(x, y, radius);
+		Lines.stroke(1);
+	},
   fillCircle(x, y, col, alpha, radius){
     Draw.color(col);
     Draw.alpha(alpha);
     Fill.circle(x, y, radius);
     Draw.color();
   },
+	fillCircle(x, y, radius){
+		Fill.circle(x, y, radius);
+	},
   splashLines(x, y, col, thickness, distance, length, count, seed){
     Draw.color(col);
     Lines.stroke(thickness);
@@ -23,9 +31,21 @@ module.exports = {
     Draw.color();
     Lines.stroke(1);
   },
+	splashLines(x, y, thickness, distance, length, count, seed){
+		Lines.stroke(thickness);
+		Angles.randLenVectors(seed, count, distance, new Floatc2(){get: (a, b) => {
+			Lines.lineAngle(x + a, y + b, Mathf.angle(a, b), length);
+		}});
+		Lines.stroke(1);
+	},
   splashCircles(x, y, col, alpha, radius, distance, count, seed){
-	Angles.randLenVectors(seed, count, distance, new Floatc2(){get: (a, b) => {
-	  this.fillCircle(x + a, y + b, col, alpha, radius);
-	}});
+    Angles.randLenVectors(seed, count, distance, new Floatc2(){get: (a, b) => {
+      this.fillCircle(x + a, y + b, col, alpha, radius);
+    }});
   },
+	splashCircles(x, y, radius, distance, count, seed){
+		Angles.randLenVectors(seed, count, distance, new Floatc2(){get: (a, b) => {
+      this.fillCircle(x + a, y + b, radius);
+    }});
+	},
 };

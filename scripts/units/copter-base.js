34cbde8a6c0b2d89e4b74a79c4577d65ee3e0365
GlennFolker <glenn.folk08@gmail.com>
2020-07-08 17:04:03 +0700

    Helicopter rendering update
    Changed almost entirely

--- scripts/units/copter-base.js
+++ scripts/units/copter-base.js
@@ -1,47 +1,40 @@
 module.exports = {
-  drawBase(base){
-    Draw.mixcol(Color.white, base.hitTime / base.hitDuration);
-    Draw.rect(base.type.region, base.x, base.y, base.rotation - 90);
-    Draw.mixcol();
-  },
-  drawRotor(base){
-    var bladeRegion = Core.atlas.isFound(base.type.rotorBladeRegion()) ?
-      base.type.rotorBladeRegion() : Core.atlas.find("mechanical-warfare-rotor-blade");
-    var topRegion = Core.atlas.isFound(base.type.rotorTopRegion()) ?
-      base.type.rotorTopRegion() : Core.atlas.find("mechanical-warfare-rotor-top");
-    var offx = Angles.trnsx(base.rotation, base.type.rotorOffset(), base.type.rotorWidth());
-    var offy = Angles.trnsy(base.rotation, base.type.rotorOffset(), base.type.rotorWidth());
-    var w = bladeRegion.getWidth() * base.type.rotorScale() * Draw.scl;
-    var h = bladeRegion.getHeight() * base.type.rotorScale() * Draw.scl;
-    var angle = Time.time() * base.type.rotorSpeed() % 360;
-    var angle2 = base.type.alternateRotor() ? 360 - (angle % 360) : 90 + angle;
-    if(base.type.isTwinBlade()){
-      for(var i = 0; i < 2; i++){
-        var sign = Mathf.signs[i];
-        var twinOffX = offx * sign;
-        var twinOffY = offy * sign;
-        Draw.rect(bladeRegion, base.x + twinOffX, base.y + twinOffY, w, h, angle);
-        Draw.rect(bladeRegion, base.x + twinOffX, base.y + twinOffY, w, h, angle2);
-        Draw.rect(topRegion, base.x + twinOffX, base.y + twinOffY);
-      }
-    }else{
-      Draw.rect(bladeRegion, base.x + offx, base.y + offy, w, h, angle);
-      Draw.rect(bladeRegion, base.x + offx, base.y + offy, w, h, angle2);
-      Draw.rect(topRegion, base.x + offx, base.y + offy);
-    }
-  },
-  drawWeapons(base){
-    for(var i = 0; i <= 1; i++){
-      var sign = Mathf.signs[i];
-      var angle = base.rotation - 90;
-      var trY = base.type.weaponOffsetY - base.type.weapon.getRecoil(base, (sign > 0));
-      var w = -sign * base.type.weapon.region.getWidth() * Draw.scl;
-      var h = base.type.weapon.region.getHeight() * Draw.scl;
-      Draw.rect(base.type.weapon.region, 
-        base.x + Angles.trnsx(angle, base.getWeapon().width * sign, trY),
-        base.y + Angles.trnsy(angle, base.getWeapon().width * sign, trY),
-        w, h, angle
-      );
-    }
-  }
+	drawBase(base){
+		Draw.mixcol(Color.white, base.hitTime / base.hitDuration);
+		Draw.rect(base.type.region, base.x, base.y, base.rotation - 90);
+		Draw.mixcol();
+	},
+	drawRotor(base){
+		att = base.type.getAttributes();
+		for(var i = 0; i < att.rotorCount; i++){
+			rotor = att.rotor[i];
+			region = rotor.bladeRegion;
+			topRegion = rotor.topRegion;
+			offx = Angles.trnsx(base.rotation - 90, rotor.width, rotor.offset);
+			offy = Angles.trnsy(base.rotation - 90, rotor.width, rotor.offset);
+			w = region.getWidth() * rotor.scale * Draw.scl;
+			h = region.getHeight() * rotor.scale * Draw.scl;
+			for(var j = 0; j < rotor.bladeCount; j++){
+				angle = ((base.id * 24) + (Time.time() * rotor.speed) + ((360 / rotor.bladeCount) * j)) % 360;
+				Draw.alpha(Vars.state.isPaused() ? 1 : Time.time() % 2);
+				Draw.rect(region, base.x + offx, base.y + offy, w, h, angle);
+			}
+			Draw.alpha(1);
+			Draw.rect(topRegion, base.x + offx, base.y + offy, base.rotation);
+		}
+	},
+	drawWeapons(base){
+		for(var i = 0; i < 2; i++){
+			var sign = Mathf.signs[i];
+			var angle = base.rotation - 90;
+			var trY = base.type.weaponOffsetY - base.type.weapon.getRecoil(base, (sign > 0));
+			var w = -sign * base.type.weapon.region.getWidth() * Draw.scl;
+			var h = base.type.weapon.region.getHeight() * Draw.scl;
+			Draw.rect(base.type.weapon.region, 
+				base.x + Angles.trnsx(angle, base.getWeapon().width * sign, trY),
+				base.y + Angles.trnsy(angle, base.getWeapon().width * sign, trY),
+				w, h, angle
+			);
+		}
+	}
 };
--- scripts/units/copter-unit.js
+++ scripts/units/copter-unit.js
@@ -41,95 +41,91 @@ serpentWeapon.shootSound = Sounds.shootSnap;
 serpentWeapon.bullet = serpentBullet;
 
 const serpentUnit = extendContent(UnitType, "serpent", {
-  load(){
-    this.weapon.load();
-    this.region = Core.atlas.find(this.name);
-  },
-  secondaryReload: function(){
-    return 40;
-  },
-  secondaryShootCone: function(){
-    return 45;
-  },
-  secondaryShootSound: function(){
-    return Sounds.missile;
-  },
-  rotorBladeRegion: function(){
-    return typeof(this.bladeRegion) !== "undefined" ? this.bladeRegion : Core.atlas.find("error");
-  },
-  rotorTopRegion: function(){
-    return typeof(this.topRegion) !== "undefined" ? this.topRegion : Core.atlas.find("error");
-  },
-  rotorScale: function(){
-    return 1.4;
-  },
-  rotorSpeed: function(){
-    return 8;
-  },
-  rotorOffset: function(){
-    return 5;
-  },
-  rotorWidth: function(){
-    return 0;
-  },
-  alternateRotor: function(){
-    return false;
-  },
-  isTwinBlade: function(){
-    return false;
-  },
+	load(){
+		this.weapon.load();
+		this.region = Core.atlas.find(this.name);
+	},
+	getAttributes(){
+		att = {
+			secondaryReload: 40,
+			secondaryShootCone: 45,
+			secondaryShootSound: Sounds.missile,
+			rotorCount: 1,
+			rotor: [],
+		}
+		att.rotor[0] = {
+			offset: 5,
+			width: 0,
+			speed: 39,
+			scale: 1.4,
+			bladeCount: 4,
+			bladeRegion: Core.atlas.find("mechanical-warfare-rotor-blade"),
+			topRegion: Core.atlas.find("mechanical-warfare-rotor-top")
+		}
+		return att;
+	}
 });
 serpentUnit.weapon = serpentWeapon;
-serpentUnit.create(prov(() => extend(HoverUnit, {
-  behavior(){
-    this.super$behavior();
-    if(typeof(this.missileTimer) === "undefined"){
-      this.missileTimer = 0;
-    }
-    if(typeof(this.currentLauncher) === "undefined"){
-      this.currentLauncher = -1;
-    }
-    if(this.target != null && this.target.getTeam().isEnemy(this.getTeam()) && Angles.near(this.angleTo(this.target), this.rotation, this.type.secondaryShootCone()) && this.dst(this.target) < serpentMissile.range()){
-      if(this.missileTimer++ >= this.type.secondaryReload()){
-        this.secondaryShoot(serpentMissile);
-		this.missileTimer = 0;
-        if(this.currentLauncher < 0){
-          this.currentLauncher = 1;
-        }else{
-          this.currentLauncher = -1;
-        }
-      }
-    }
-  },
-  secondaryShoot(bullet){
-    this.offx = this.x + Angles.trnsx(this.rotation - 90, this.getWeapon().width * this.currentLauncher / 2, 1);
-    this.offy = this.y + Angles.trnsy(this.rotation - 90, this.getWeapon().width * this.currentLauncher / 2, 1);
-    Bullet.create(bullet, this, this.getTeam(), this.offx, this.offy, this.rotation, 1 - Mathf.random(0.1), 1);
-	this.type.secondaryShootSound().at(this.x, this.y, Mathf.random(0.9, 1.1));
-	Effects.effect(bullet.shootEffect, this.offx, this.offy, this.rotation);
-	Effects.effect(bullet.smokeEffect, this.offx, this.offy, this.rotation);
-  },
-  draw(){
-	this.drawWeapons();
-    copterLib.drawBase(this);
-  },
-  drawWeapons(){
-    copterLib.drawWeapons(this);
-  },
-  drawStats(){
-	this.super$drawStats();
-	copterLib.drawRotor(this);
-  },
-  drawEngine(){}
-})));
+serpentUnit.create(prov(() => {
+	const base = extend(HoverUnit, {
+		behavior(){
+			this.super$behavior();
+			att = this.type.getAttributes();
+			if(typeof(this.missileTimer) === "undefined"){
+				this.missileTimer = 0;
+			}
+			if(typeof(this.currentLauncher) === "undefined"){
+				this.currentLauncher = 0;
+			}
+			if(
+				this.target != null &&
+				this.target.getTeam().isEnemy(this.getTeam()) &&
+				Angles.near(this.angleTo(this.target), this.rotation, att.secondaryShootCone) &&
+				this.dst(this.target) < serpentMissile.range()
+			){
+				if(this.missileTimer++ >= att.secondaryReload){
+					this.secondaryShoot(serpentMissile);
+					this.missileTimer = 0;
+					if(this.currentLauncher < 0){
+						this.currentLauncher = 1;
+					}else{
+						this.currentLauncher = -1;
+					}
+				}
+			}
+		},
+		secondaryShoot(bullet){
+			att = this.type.getAttributes();
+			this.offx = this.x + Angles.trnsx(this.rotation - 90, this.getWeapon().width * this.currentLauncher / 2, 1);
+			this.offy = this.y + Angles.trnsy(this.rotation - 90, this.getWeapon().width * this.currentLauncher / 2, 1);
+			Bullet.create(bullet, this, this.getTeam(), this.offx, this.offy, this.rotation, 1 - Mathf.random(0.1), 1);
+			att.secondaryShootSound.at(this.x, this.y, Mathf.random(0.9, 1.1));
+			Effects.effect(bullet.shootEffect, this.offx, this.offy, this.rotation);
+			Effects.effect(bullet.smokeEffect, this.offx, this.offy, this.rotation);
+		},
+		draw(){
+			this.drawWeapons();
+			copterLib.drawBase(this);
+		},
+		drawWeapons(){
+			copterLib.drawWeapons(this);
+		},
+		drawStats(){
+			this.super$drawStats();
+			copterLib.drawRotor(this);
+		},
+		drawEngine(){}
+	});
+	return base;
+}));
 
 const serpentFactory = extendContent(UnitFactory, "serpent-factory", {
-  load(){
-    this.region = Core.atlas.find(this.name);
-    this.topRegion = Core.atlas.find("clear");
-  },
-  generateIcons: function(){
-    return [Core.atlas.find(this.name)];
-  }
+	load(){
+		this.region = Core.atlas.find(this.name);
+		this.topRegion = Core.atlas.find("clear");
+	},
+	generateIcons: function(){
+		return [Core.atlas.find(this.name)];
+	}
 });
 serpentFactory.unitType = serpentUnit;

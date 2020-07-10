ca21832d2c0d832474fbdccfea0deca8fbf48e0f
GlennFolker <glenn.folk08@gmail.com>
2020-07-10 20:55:53 +0700

    Viper addition (todo: viper factory sprite)

--- scripts/units/copter-unit.js
+++ scripts/units/copter-unit.js
@@ -1,7 +1,7 @@
-const copterLib = require("units/copter-base");
-importPackage(Packages.mindustry.net);
-importPackage(Packages.mindustry.io);
-tempBuffer = java.nio.ByteBuffer.allocate(4096);
+const copterLib = require("mechanical-warfare/units/copter-base");
+const multiWeap = require("mechanical-warfare/units/multi-weapon-base");
+const plib = require("mechanical-warfare/plib");
+const elib = require("mechanical-warfare/effectlib");
 
 // Serpent
 const serpentBullet = extend(BasicBulletType, {});
@@ -21,7 +21,7 @@ serpentMissile.speed = 2.8;
 serpentMissile.lifetime = 50;
 serpentMissile.damage = 24;
 serpentMissile.drag = -0.01;
-serpentMissile.homingPower = 0.075;
+serpentMissile.homingPower = 2;
 serpentMissile.homingRange = 120;
 serpentMissile.shootEffect = Fx.shootBig;
 serpentMissile.smokeEffect = Fx.shootBigSmoke;
@@ -51,6 +51,10 @@ const serpentUnit = extendContent(UnitType, "serpent", {
 	getAttributes(){
 		att = {
 			weaponCount: 1,
+			rotateWeapon: [],
+			weaponAngles: [],
+			shootCone: [],
+			weaponOffsetY: [],
 			weapon: [],
 			rotorCount: 1,
 			rotor: [],
@@ -58,13 +62,17 @@ const serpentUnit = extendContent(UnitType, "serpent", {
 		att.rotor[0] = {
 			offset: 5,
 			width: 0,
-			speed: 39,
+			speed: 39.11,
 			scale: 1.4,
 			bladeCount: 4,
 			bladeRegion: Core.atlas.find("mechanical-warfare-rotor-blade"),
 			topRegion: Core.atlas.find("mechanical-warfare-rotor-top")
 		}
-		att.weapon[0] = copterLib.weapon("serpent-launcher", 0);
+		att.weaponAngles[0] = [0.0, 0.0];
+		att.shootCone[0] = 30;
+		att.weaponOffsetY[0] = 0.0;
+		att.rotateWeapon[0] = false;
+		att.weapon[0] = multiWeap.newWeapon("serpent-launcher", 0, null, null);
 		att.weapon[0].width = 4.25;
 		att.weapon[0].length = 1;
 		att.weapon[0].recoil = 1.5;
@@ -85,7 +93,7 @@ serpentUnit.create(prov(() => {
 			this.super$update();
 			if(this.target !== null){
 				if(this.target.getTeam().isEnemy(this.getTeam())){
-					copterLib.updateWeapons(this);
+					multiWeap.updateWeapons(this);
 				}
 			}
 		},
@@ -94,7 +102,7 @@ serpentUnit.create(prov(() => {
 			copterLib.drawBase(this);
 		},
 		drawWeapons(){
-			copterLib.drawWeapons(this);
+			multiWeap.drawWeapons(this);
 		},
 		drawStats(){
 			this.super$drawStats();
@@ -136,37 +144,216 @@ const serpentFactory = extendContent(UnitFactory, "serpent-factory", {
 });
 serpentFactory.unitType = serpentUnit;
 
-/*// Viper
+// Viper
 const viperBullet = extend(BasicBulletType, {});
 viperBullet.bulletWidth = 9;
 viperBullet.bulletHeight = 14;
-viperBullet.speed = 8;
-viperBullet.lifetime = 16;
-viperBullet.damage = 14;
+viperBullet.frontColor = plib.frontColorCyan;
+viperBullet.backColor = plib.backColorCyan;
+viperBullet.speed = 6;
+viperBullet.lifetime = 21;
+viperBullet.damage = 12;
 viperBullet.shootEffect = Fx.shootBig;
 viperBullet.smokeEffect = Fx.shootBigSmoke;
 viperBullet.hitEffect = Fx.hitBulletBig;
 
-const viperGun = extendContent(Weapon, "viper-gun", {
+const viperGun = extendContent(Weapon, "viper-machine-gun", {
 	load(){
-		this.region = Core.atlas.find("clear");
+		this.region = Core.atlas.find("mechanical-warfare-viper-machine-gun-equip");
 	}
 });
-viperGun.reload = 10;
+viperGun.width = 4;
+viperGun.length = 19.25;
+viperGun.reload = 16;
 viperGun.alternate = true;
 viperGun.recoil = 1.5;
 viperGun.shake = 0;
 viperGun.inaccuracy = 3;
 viperGun.ejectEffect = Fx.shellEjectSmall;
-viperGun.shootSound = Sounds.shootSBig;
+viperGun.shootSound = Sounds.shootBig;
 viperGun.bullet = viperBullet;
 
-const viper = extendContent(UnitType, "viper", {
+const viperLaser = extend(BasicBulletType, {
+	draw(b){
+		col = plib.frontColorCyan;
+		x = b.getData().target.x;
+		y = b.getData().target.y;
+		angle = Angles.angle(b.x, b.y, x, y);
+		dst = Mathf.dst(b.x, b.y, x, y);
+		Lines.precise(true);
+		Lines.stroke(b.fout() * 2.8);
+		Draw.color(col, 0.3 + b.fout() * 0.7);
+		Lines.lineAngle(b.x, b.y, angle, dst);
+		Lines.stroke(1);
+		Lines.precise(false);
+		Drawf.tri(x, y, b.fout() * 2.8, 10, angle);
+		Draw.color();
+	},
+	init(b){
+		if(typeof(b) !== "undefined"){
+			this.super$init(b);
+			unit = b.getOwner();
+			b.setData({target: unit.target});
+			x = b.getData().target.x;
+			y = b.getData().target.y;
+			angle = Angles.angle(b.x, b.y, x, y);
+			dst = Mathf.dst(b.x, b.y, x, y);
+			Damage.collideLine(b, b.getTeam(), this.hitEffect, b.x, b.y, angle, dst);
+		}
+	},
+	range(){
+		return 100;
+	}
+});
+viperLaser.pierce = true;
+viperLaser.damage = 30;
+viperLaser.despawnEffect = Fx.none;
+viperLaser.hitEffect = Fx.hitBulletSmall;
+viperLaser.lifetime = 25;
+viperLaser.speed = 0.001;
+viperLaser.collides = false;
+viperLaser.hitTiles = false;
+viperLaser.collidesTiles = false;
+viperLaser.collidesAir = false;
+viperLaser.collidesTeam = false;
+
+const viperMissile = extend(MissileBulletType, {});
+viperMissile.frontColor = plib.frontColorCyan;
+viperMissile.backColor = plib.backColorCyan;
+viperMissile.bulletWidth = 9;
+viperMissile.bulletHeight = 12;
+viperMissile.speed = 3.2;
+viperMissile.lifetime = 55;
+viperMissile.damage = 16;
+viperMissile.drag = -0.01;
+viperMissile.homingPower = 2;
+viperMissile.homingRange = 120;
+viperMissile.shootEffect = Fx.shootBig;
+viperMissile.smokeEffect = Fx.shootBigSmoke;
+viperMissile.hitEffect = Fx.blastExplosion;
+viperMissile.trailColor = viperMissile.backColor;
+
+const viperUnit = extendContent(UnitType, "viper", {
 	load(){
 		this.weapon.load();
 		this.region = Core.atlas.find(this.name);
 	},
 	getAttributes(){
+		att = {
+			weaponCount: 2,
+			rotateWeapon: [],
+			weaponAngles: [],
+			shootCone: [],
+			weaponOffsetY: [],
+			weapon: [],
+			rotorCount: 2,
+			rotor: [],
+		}
+		for(var i = 0; i < att.rotorCount; i++){
+			sign = Mathf.signs[i];
+			att.rotor[i] = {
+				offset: 4 + sign * 10.5,
+				width: 0,
+				speed: 39.11,
+				scale: 1.2,
+				bladeCount: 3,
+				bladeRegion: Core.atlas.find("mechanical-warfare-rotor-blade2"),
+				topRegion: Core.atlas.find("mechanical-warfare-rotor-top")
+			}
+		}
+		for(var i = 0; i < att.weaponCount; i++){
+			att.weaponAngles[i] = [0.0, 0.0];
+			att.shootCone[i] = 30;
+			att.weaponOffsetY[i] = 0.0;
+			att.rotateWeapon[i] = false;
+		}
+		att.weapon[0] = multiWeap.newWeapon("viper-lasergun", 0, (weap, shooter, x, y, rotation, left) => {
+			weap.shootSound.at(x, y, Mathf.random(1.8, 2.0));
+			Angles.shotgun(weap.shots, weap.spacing, rotation, new Floatc(){get: f => {
+				weap.bulletB(shooter, x, y, f);
+			}});
+		}, null);
+		att.weapon[0].width = 7;
+		att.weapon[0].length = 8;
+		att.weapon[0].recoil = 2;
+		att.weapon[0].alternate = false;
+		att.weapon[0].inaccuracy = 0;
+		att.weapon[0].reload = 80;
+		att.weapon[0].ejectEffect = Fx.none;
+		att.weapon[0].shootSound = Sounds.shotgun;
+		att.weapon[0].bullet = viperLaser;
+		
+		att.weapon[1] = multiWeap.newWeapon("viper-launcher", 1, null, null);
+		att.weapon[1].width = 7;
+		att.weapon[1].length = 3.5;
+		att.weapon[1].recoil = 2.3;
+		att.weapon[1].shots = 3;
+		att.weapon[1].alternate = true;
+		att.weapon[1].inaccuracy = 3;
+		att.weapon[1].reload = 60;
+		att.weapon[1].ejectEffect = Fx.shellEjectBig;
+		att.weapon[1].shootSound = Sounds.missile;
+		att.weapon[1].bullet = viperMissile;
+		return att;
 	}
 });
-viper.weapon = viperGun;*/
+viperUnit.weapon = viperGun;
+viperUnit.create(prov(() => {
+	const base = extend(HoverUnit, {
+		update(){
+			this.super$update();
+			if(this.target !== null){
+				if(this.target.getTeam().isEnemy(this.getTeam())){
+					multiWeap.updateWeapons(this);
+				}
+			}
+		},
+		draw(){
+			this.drawWeapons();
+			copterLib.drawBase(this);
+		},
+		drawWeapons(){
+			multiWeap.drawWeapons(this);
+		},
+		drawStats(){
+			this.super$drawStats();
+			copterLib.drawRotor(this);
+		},
+		drawEngine(){},
+		getTimer2(){
+			return this._timer;
+		},
+		setTimer2(val){
+			this._timer = val;
+		},
+		getShootTimer2(index, left){
+			return left ? this.getShootTimers2()[index].timerShootLeft : this.getShootTimers2()[index].timerShootRight;
+		},
+		getShootTimers2(){
+			return this._timerShoot;
+		},
+		setShootTimers2(arr){
+			this._timerShoot = arr;
+		}
+	});
+	base.setTimer2(new Interval(4));
+	base.setShootTimers2([{
+		timerShootLeft: 0,
+		timerShootRight: 1
+	}, {
+		timerShootLeft: 2,
+		timerShootRight: 3
+	}]);
+	return base;
+}));
+
+const viperFactory = extendContent(UnitFactory, "viper-factory", {
+	load(){
+		this.region = Core.atlas.find(this.name);
+		this.topRegion = Core.atlas.find("clear");
+	},
+	generateIcons: function(){
+		return [Core.atlas.find(this.name)];
+	}
+});
+viperFactory.unitType = viperUnit;
---
+++ scripts/units/multi-weapon-base.js
@@ -0,0 +1,195 @@
+importPackage(Packages.mindustry.net);
+importPackage(Packages.mindustry.io);
+importPackage(Packages.arc.util.pooling);
+tempBuffer = java.nio.ByteBuffer.allocate(4096);
+module.exports = {
+	drawWeapons(base){
+		att = base.type.getAttributes();
+		// Primary weapon
+		Draw.mixcol(Color.white, base.hitTime / base.hitDuration);
+		for(var i = 0; i < 2; i++){
+			sign = Mathf.signs[i];
+			tra = base.rotation - 90;
+			trY = base.type.weaponOffsetY - base.type.weapon.getRecoil(base, (sign > 0));
+			w = -sign * base.type.weapon.region.getWidth() * Draw.scl;
+			h = base.type.weapon.region.getHeight() * Draw.scl;
+			wx = base.x + Angles.trnsx(tra, base.getWeapon().width * sign, trY);
+			wy = base.y + Angles.trnsy(tra, base.getWeapon().width * sign, trY);
+			if(base.type.rotateWeapon){
+				wi = (sign + 1) / 2;
+				Draw.rect(base.type.weapon.region, wx, wy, w, h, base.weaponAngles[wi] - 90);
+			}else{
+				Draw.rect(base.type.weapon.region, wx, wy, w, h, tra);
+			}
+		}
+		
+		// Custom weapons
+		for(var i = 0; i < att.weaponCount; i++){
+			weap = att.weapon[i];
+			region = weap.getRegion();
+			tra = base.rotation - 90;
+			for(var j = 0; j < 2; j++){
+				sign = Mathf.signs[j];
+				trY = att.weaponOffsetY[i] - weap.getRecoil(base, (sign > 0));
+				w = -sign * region.getWidth() * Draw.scl;
+				h = region.getHeight() * Draw.scl;
+				wx = base.x + Angles.trnsx(tra, weap.width * sign, trY);
+				wy = base.y + Angles.trnsy(tra, weap.width * sign, trY);
+				if(att.rotateWeapon[i]){
+					wi = (sign + 1) / 2;
+					Draw.rect(region, wx, wy, w, h, att.weaponAngles[i][wi] - 90);
+				}else{
+					Draw.rect(region, wx, wy, w, h, tra);
+				}
+			}
+		}
+		Draw.mixcol();
+	},
+	updateWeapons(base){
+		att = base.type.getAttributes();
+		for(var i = 0; i < att.weaponCount; i++){
+			weap = att.weapon[i];
+			ammo = weap.bullet;
+			if(
+				(Angles.near(base.angleTo(base.target), base.rotation, att.shootCone[i]) || weap.ignoreRotation) &&
+				base.dst(base.target) < ammo.range()
+			){
+				if(att.rotateWeapon[i]){
+					for(var j = 0; j < 2; j++){
+						left = Mathf.booleans[j];
+						wi = Mathf.num(left);
+						wx = base.x + Angles.trnsx(base.rotation - 90, weap.width * Mathf.sign(left));
+						wy = base.y = Angles.trnsy(base.rotation - 90, weap.width * Mathf.sign(left));
+						if(base.target.getTeam().isEnemy(base.getTeam())){
+							if(base.target.withinDst(base.x, base.y, ammo.range())){
+								att.weaponAngles[i][wi] = Mathf.slerpDelta(att.weaponAngles[i][wi], Angles.angle(wx, wy, base.target.getX(), base.target.getY()), 0.1);
+							}else{
+								att.weaponAngles[i][wi] = Mathf.slerpDelta(att.weaponAngles[i][wi], base.rotation - 90, 0.1);
+							}
+						}
+						Tmp.v2.trns(att.weaponAngles[i][wi], weap.length);
+						weap.updateB(base, wx + Tmp.v2.x, wy + Tmp.v2.y, att.weaponAngles[i][wi], left);
+					}
+				}else{
+					to = Predict.intercept(base, base.target, ammo.speed);
+					weap.update(base, to.x, to.y);
+				}
+			}
+		}
+	},
+	newWeapon(name, index, customShoot, customBullet){
+		const temp = extendContent(Weapon, name, {
+			getRegion(){
+				return Core.atlas.find("mechanical-warfare-" + name + "-equip");
+			},
+			update(shooter, pointerX, pointerY){
+				for(var i = 0; i < 2; i++){
+					j = Mathf.booleans[i];
+					Tmp.v1.set(pointerX, pointerY).sub(shooter.getX(), shooter.getY());
+					if(Tmp.v1.len() < this.minPlayerDist){Tmp.v1.setLength(this.minPlayerDist);}
+					cx = Tmp.v1.x + shooter.getX();
+					cy = Tmp.v1.y + shooter.getY();
+					ang = Tmp.v1.angle();
+					Tmp.v1.trns(ang - 90.0, this.width * Mathf.sign(j), this.length + Mathf.range(this.lengthRand));
+					this.updateB(shooter,
+						shooter.getX() + Tmp.v1.x,
+						shooter.getY() + Tmp.v1.y,
+						Angles.angle(shooter.getX() + Tmp.v1.x, shooter.getY() + Tmp.v1.y, cx, cy),
+						j
+					);
+				}
+			},
+			updateB(shooter, mountX, mountY, angle, left){
+				if(shooter.getTimer2().get(shooter.getShootTimer2(index, left), this.reload)){
+					if(this.alternate){
+						shooter.getTimer2().reset(shooter.getShootTimer2(index, !left), this.reload / 2.0);
+					}
+					this.shootDirectB(shooter, mountX - shooter.getX(), mountY - shooter.getY(), angle, left);
+				}
+			},
+			shootDirectB(shooter, offsetX, offsetY, rotation, left){
+				x = shooter.getX() + offsetX;
+				y = shooter.getY() + offsetY;
+				baseX = shooter.getX();
+				baseY = shooter.getY();
+				
+				weap = shooter.type.getAttributes().weapon[index];
+				if(customShoot == null){
+					weap.shootSound.at(x, y, Mathf.random(0.8, 1.0));
+					sequenceNum = 0;
+					if(weap.shotDelay > 0.01){
+						Angles.shotgun(weap.shots, weap.spacing, rotation, new Floatc(){get: f => {
+							Time.run(sequenceNum * weap.shotDelay, run(() => {
+								weap.bulletB(shooter, x, y, f + Mathf.range(weap.inaccuracy));
+							}));
+							sequenceNum++;
+						}});
+					}else{
+						Angles.shotgun(weap.shots, weap.spacing, rotation, new Floatc(){get: f => {
+							weap.bulletB(shooter, x, y, f + Mathf.range(weap.inaccuracy));
+						}});
+					}
+				}else{
+					customShoot(weap, shooter, x, y, rotation, left);
+				}
+				ammo = weap.bullet;
+				Tmp.v1.trns(rotation + 180, ammo.recoil);
+				shooter.velocity().add(Tmp.v1);
+				Tmp.v1.trns(rotation, 3);
+				Effects.effect(weap.ejectEffect, x, y, rotation * -Mathf.sign(left));
+				Effects.effect(ammo.shootEffect, x + Tmp.v1.x, y + Tmp.v1.y, rotation, shooter);
+				Effects.effect(ammo.smokeEffect, x + Tmp.v1.x, y + Tmp.v1.y, rotation, shooter);
+				shooter.getTimer2().get(shooter.getShootTimer2(index, left), weap.reload);
+				// Manual Synchronization
+				if(shooter instanceof BaseUnit){
+					this.syncUnit(shooter, offsetX, offsetY, rotation, left);
+				}else{
+					this.syncPlayer(shooter, offsetX, offsetY, rotation, left);
+				}
+			},
+			syncUnit(shooter, x, y, rotation, left){
+				if(Vars.net.client()){return;}
+				if(shooter == null){return;}
+				if(shooter == Vars.player){return;}
+				packet = Pools.obtain(Packets.InvokePacket, prov(() => new Packets.InvokePacket));
+				packet.writeBuffer = tempBuffer;
+				packet.priority = 0;
+				packet.type = 19;
+				tempBuffer.position(0);
+				TypeIO.writeShooter(tempBuffer, shooter);
+				tempBuffer.putFloat(x);
+				tempBuffer.putFloat(y);
+				tempBuffer.putFloat(rotation);
+				tempBuffer.put(left ? 0 : 1);
+				packet.writeLength = tempBuffer.position();
+				Vars.net.send(packet, Net.SendMode.udp);
+			},
+			syncPlayer(shooter, x, y, rotation, left){
+				if(Vars.net.client()){return;}
+				if(shooter == null){return;}
+				if(shooter == Vars.player){return;}
+				packet = Pools.obtain(Packets.InvokePacket, prov(() => new Packets.InvokePacket));
+				packet.writeBuffer = tempBuffer;
+				packet.priority = 0;
+				packet.type = 32;
+				tempBuffer.position(0);
+				TypeIO.writePlayer(tempBuffer, shooter);
+				tempBuffer.putFloat(x);
+				tempBuffer.putFloat(y);
+				tempBuffer.putFloat(rotation);
+				tempBuffer.put(left ? 0 : 1);
+				packet.writeLength = tempBuffer.position();
+				Vars.net.send(packet, Net.SendMode.udp);
+			},
+			bulletB(owner, x, y, angle){
+				if(owner == null){return;}
+				Tmp.v1.trns(angle, 3.0);
+				Bullet.create(this.bullet, owner, owner.getTeam(), x + Tmp.v1.x, y + Tmp.v1.y, angle, 1.0 - this.velocityRnd + Mathf.random(this.velocityRnd));
+			},
+			getRecoil(shooter, left){
+				return (1.0 - Mathf.clamp(shooter.getTimer2().getTime(shooter.getShootTimer2(index, left)) / this.reload)) * this.recoil;
+			}
+		});
+		return temp;
+	}
+}

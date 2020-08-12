43754519d1cf60133b51f064a50de3ea1eace2ea
GlennFolker <glenn.folk08@gmail.com>
2020-08-12 20:26:59 +0700

    Upsylon Mech rework

--- scripts/mechs/upsylon-mech.js
+++ scripts/mechs/upsylon-mech.js
@@ -1,6 +1,7 @@
 const elib = require("mechanical-warfare/effectlib");
 const plib = require("mechanical-warfare/plib");
 const bulletLib = require("mechanical-warfare/bulletlib");
+const trailLib = require("mechanical-warfare/traillib");
 
 const shotgunRound = bulletLib.bullet(BasicBulletType, 6, 9, 0.3, 0, 9, 0, -1, 0, 6, 17, null, null, null, null, null);
 shotgunRound.frontColor = plib.frontColorCyan;
@@ -14,7 +15,7 @@ const shotgun = extendContent(Weapon, "shotgun", {
     this.region = Core.atlas.find("mechanical-warfare-shotgun-equip");
   }
 });
-shotgun.width = 5;
+shotgun.width = 6;
 shotgun.length = 5.5;
 shotgun.recoil = 3;
 shotgun.reload = 30;
@@ -28,20 +29,25 @@ shotgun.velocityRnd = 0.3;
 shotgun.shootSound = Sounds.shootBig;
 shotgun.bullet = shotgunRound;
 
-const upsylonPlasma = bulletLib.bullet(BasicBulletType, 1, 1, 0, 0.08, Vars.state.rules.playerDamageMultiplier * 15, Vars.state.rules.playerDamageMultiplier * 30, 20, 0, 12, 48, cons(b => {
+const upsylonPlasma = bulletLib.bullet(BasicBulletType, 1, 1, 0, 0.08, Vars.state.rules.playerDamageMultiplier * 15, Vars.state.rules.playerDamageMultiplier * 30, 20, 0, 14, 48, cons(b => {
 	elib.fillCircle(b.x, b.y, Pal.lancerLaser, 1, 6);
-}), cons(b => {
-	Effects.effect(upsylonPlasma.trailEffect, b.x, b.y, b.rot());
-}), null, null, null);
+	b.getData().trail.draw(Pal.lancerLaser, 6);
+}), null, cons(b => {
+	Core.app.post(run(() => {
+		if(b == null || b.getData() == null)return;
+		b.getData().trail.update(b.x, b.y);
+	}));
+}), null, cons(b => {
+	b.setData({
+		trail: trailLib.newTrail(10)
+	});
+}));
 upsylonPlasma.homingRange = upsylonPlasma.range();
-upsylonPlasma.homingPower = 0.001;
+upsylonPlasma.homingPower = 1;
 upsylonPlasma.shootEffect = Fx.shootBig;
 upsylonPlasma.smokeEffect = Fx.shootBigSmoke;
-upsylonPlasma.trailEffect = newEffect(30, e => {
-  elib.fillCircle(e.x, e.y, Pal.lancerLaser, 1, Mathf.lerp(6, 0.2, e.fin()));
-});
 upsylonPlasma.hitEffect = newEffect(16, e => {
-  e.scaled(1.2, cons(i => {
+  e.scaled(8, cons(i => {
 	elib.outlineCircle(e.x, e.y, Pal.lancerLaser, i.fout() * 6, 1 + i.fin() * (upsylonPlasma.splashDamageRadius + 6));
   }));
   elib.fillCircle(e.x, e.y, Pal.lancerLaser, 0.2 + e.fout() * 0.8, Mathf.lerp(upsylonPlasma.splashDamageRadius, 0.2, e.fin()));
@@ -82,7 +88,7 @@ upsylon.engineColor = plib.engineColorCyan;
 upsylon.engineOffset = 5.6;
 upsylon.health = 250;
 upsylon.buildPower = 1.8;
-upsylon.weaponOffsetX = 2;
+upsylon.weaponOffsetX = 6;
 upsylon.weapon = shotgun;
 
 const upsylonPad = extendContent(MechPad, "upsylon-mech-pad", {});

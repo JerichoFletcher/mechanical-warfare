4fce73535d164faec3593c9a85f3070d0a22c1eb
GlennFolker <glenn.folk08@gmail.com>
2020-10-18 14:22:52 +0700

    ~~nieto~~ neato

--- mw/src/mw/content/MWSounds.java
+++ mw/src/mw/content/MWSounds.java
@@ -1,60 +1,81 @@
 package mw.content;
 
 import arc.*;
+import arc.assets.*;
 import arc.audio.*;
 import arc.mock.*;
+import mindustry.*;
+import mindustry.mod.*;
 
 public class MWSounds{
     public static Sound
 
-    blowshot = new MockSound(),
-    fireAuraActive = new MockSound(),
-    heatrayActive = new MockSound(),
-    hypernovaActive = new MockSound(),
-    hypernovaShoot = new MockSound(),
-    quakemk2shot = new MockSound(),
-    quakeshot = new MockSound(),
-    supernovaActive = new MockSound(),
-    supernovaShoot = new MockSound(),
-    voltmeterActive = new MockSound(),
-    voltmeterIdle = new MockSound();
+    blowshot,
+    fireAuraActive,
+    heatrayActive,
+    hypernovaActive,
+    hypernovaShoot,
+    quakemk2shot,
+    quakeshot,
+    supernovaActive,
+    supernovaShoot,
+    voltmeterActive,
+    voltmeterIdle;
 
     public static void load(){
-        Core.assets.load("sounds/blowshot.ogg", Sound.class).loaded = sound -> blowshot = (Sound)sound;
-        Core.assets.load("sounds/fireAuraActive.ogg", Sound.class).loaded = sound -> fireAuraActive = (Sound)sound;
-        Core.assets.load("sounds/heatrayActive.ogg", Sound.class).loaded = sound -> heatrayActive = (Sound)sound;
-        Core.assets.load("sounds/hypernovaActive.ogg", Sound.class).loaded = sound -> hypernovaActive = (Sound)sound;
-        Core.assets.load("sounds/hypernovaShoot.ogg", Sound.class).loaded = sound -> hypernovaShoot = (Sound)sound;
-        Core.assets.load("sounds/quakemk2shot.ogg", Sound.class).loaded = sound -> quakemk2shot = (Sound)sound;
-        Core.assets.load("sounds/quakeshot.ogg", Sound.class).loaded = sound -> quakeshot = (Sound)sound;
-        Core.assets.load("sounds/supernovaActive.ogg", Sound.class).loaded = sound -> supernovaActive = (Sound)sound;
-        Core.assets.load("sounds/supernovaShoot.ogg", Sound.class).loaded = sound -> supernovaShoot = (Sound)sound;
-        Core.assets.load("sounds/voltmeterActive.ogg", Sound.class).loaded = sound -> voltmeterActive = (Sound)sound;
-        Core.assets.load("sounds/voltmeterIdle.ogg", Sound.class).loaded = sound -> voltmeterIdle = (Sound)sound;
+        blowshot = loadSound("blowshot");
+        fireAuraActive = loadSound("fireAuraActive");
+        heatrayActive = loadSound("heatrayActive");
+        hypernovaActive = loadSound("hypernovaActive");
+        hypernovaShoot = loadSound("hypernovaShoot");
+        quakemk2shot = loadSound("quakemk2shot");
+        quakeshot = loadSound("quakeshot");
+        supernovaActive = loadSound("supernovaActive");
+        supernovaShoot = loadSound("supernovaShoot");
+        voltmeterActive = loadSound("voltmeterActive");
+        voltmeterIdle = loadSound("voltmeterIdle");
     }
 
     public static void dispose(){
-        Core.assets.unload("sounds/blowshot.ogg");
-        blowshot = null;
-        Core.assets.unload("sounds/fireAuraActive.ogg");
-        fireAuraActive = null;
-        Core.assets.unload("sounds/heatrayActive.ogg");
-        heatrayActive = null;
-        Core.assets.unload("sounds/hypernovaActive.ogg");
-        hypernovaActive = null;
-        Core.assets.unload("sounds/hypernovaShoot.ogg");
-        hypernovaShoot = null;
-        Core.assets.unload("sounds/quakemk2shot.ogg");
-        quakemk2shot = null;
-        Core.assets.unload("sounds/quakeshot.ogg");
-        quakeshot = null;
-        Core.assets.unload("sounds/supernovaActive.ogg");
-        supernovaActive = null;
-        Core.assets.unload("sounds/supernovaShoot.ogg");
-        supernovaShoot = null;
-        Core.assets.unload("sounds/voltmeterActive.ogg");
-        voltmeterActive = null;
-        Core.assets.unload("sounds/voltmeterIdle.ogg");
-        voltmeterIdle = null;
+        blowshot = disposeSound("blowshot");
+        fireAuraActive = disposeSound("fireAuraActive");
+        heatrayActive = disposeSound("heatrayActive");
+        hypernovaActive = disposeSound("hypernovaActive");
+        hypernovaShoot = disposeSound("hypernovaShoot");
+        quakemk2shot = disposeSound("quakemk2shot");
+        quakeshot = disposeSound("quakeshot");
+        supernovaActive = disposeSound("supernovaActive");
+        supernovaShoot = disposeSound("supernovaShoot");
+        voltmeterActive = disposeSound("voltmeterActive");
+        voltmeterIdle = disposeSound("voltmeterIdle");
+    }
+
+    static Sound loadSound(String n){
+        if(Vars.headless) return new MockSound();
+
+        String name = "sounds/" + n;
+        String path = Vars.tree.get(name + ".ogg").exists() && !Vars.ios ? name + ".ogg" : name + ".mp3";
+
+        if(Core.assets.contains(path, Sound.class)) return Core.assets.get(path, Sound.class);
+
+        ModLoadingSound sound = new ModLoadingSound();
+
+        AssetDescriptor<?> desc = Core.assets.load(path, Sound.class);
+        desc.loaded = result -> sound.sound = (Sound)result;
+        desc.errored = Throwable::printStackTrace;
+
+        return sound;
+    }
+
+    static Sound disposeSound(String n){
+        String name = "sounds/" + n;
+        String path = Vars.tree.get(name + ".ogg").exists() && !Vars.ios ? name + ".ogg" : name + ".mp3";
+
+        if(Core.assets.contains(path, Sound.class)){
+            Core.assets.get(path, Sound.class).dispose();
+            Core.assets.unload(path);
+        }
+
+        return null;
     }
 }
